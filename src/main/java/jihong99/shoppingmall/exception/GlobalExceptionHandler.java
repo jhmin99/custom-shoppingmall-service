1c5ac374e3230fd83e69de14ac1fa53cb9808269
jhmin99 <mjh012399@gmail.com>
2024-03-09 14:12:51 +0900

    fix : Delete the existing MethodArgumentNotValid method and implement the MethodArgumentNotValid function of ResponseEntityExceptionHandler by overriding it to fix BeanCreationException.
    MethodArgumentNotValid 예외는 이미 ResponseEntityExceptionHandler에서 처리를 하고 있는데 @ExceptionHandler(MethodArgumentNotValidException.class)로 어노테이션을 단 메서드와 중복으로 예외처리하기 때문에 @ControllerAdvice에 의해 bean객체로 생성되지를 못하고 BeanCreationException이 발생하게됨

--- src/main/java/jihong99/shoppingmall/exception/GlobalExceptionHandler.java
+++ src/main/java/jihong99/shoppingmall/exception/GlobalExceptionHandler.java
@@ -1,8 +1,12 @@
 package jihong99.shoppingmall.exception;
 
 import jihong99.shoppingmall.dto.ErrorResponseDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,6 +14,10 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -25,14 +33,16 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(errorResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponseDto> handleGlobalMethodArgumentNotValidException(MethodArgumentNotValidException methodArgumentNotValidException, WebRequest webRequest) {
-        ErrorResponseDto errorResponseDTO = new ErrorResponseDto(
-                webRequest.getDescription(false),
-                HttpStatus.INTERNAL_SERVER_ERROR,
-                methodArgumentNotValidException.getMessage(),
-                LocalDateTime.now()
-        );
-        return new ResponseEntity<>(errorResponseDTO, HttpStatus.BAD_REQUEST);
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String, String> validationErrors = new HashMap<>();
+        List<ObjectError> validationErrorList = ex.getBindingResult().getAllErrors();
+
+        validationErrorList.forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String validationMsg = error.getDefaultMessage();
+            validationErrors.put(fieldName, validationMsg);
+        });
+        return new ResponseEntity<>(validationErrors, HttpStatus.BAD_REQUEST);
     }
 }
