5cb385a07d9cc1699afb3981d83058dc0d385c67
jhmin99 <mjh012399@gmail.com>
2024-03-15 18:22:51 +0900

    feat: change @Date to @Getter

--- src/main/java/jihong99/shoppingmall/dto/ResponseDto.java
+++ src/main/java/jihong99/shoppingmall/dto/ResponseDto.java
@@ -1,9 +1,9 @@
 package jihong99.shoppingmall.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Getter
 @AllArgsConstructor
 public class ResponseDto {
 
