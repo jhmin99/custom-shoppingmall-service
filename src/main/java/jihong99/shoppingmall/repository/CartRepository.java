c3eeda15fd93fd3d2e6d4d5c173df83d20129488
jhmin99 <mjh012399@gmail.com>
2024-03-15 13:18:21 +0900

    feat: add @repository

--- src/main/java/jihong99/shoppingmall/repository/CartRepository.java
+++ src/main/java/jihong99/shoppingmall/repository/CartRepository.java
@@ -2,6 +2,8 @@ package jihong99.shoppingmall.repository;
 
 import jihong99.shoppingmall.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
 }
