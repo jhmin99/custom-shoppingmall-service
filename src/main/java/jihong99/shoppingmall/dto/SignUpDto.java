c44e8962de1fedbe66e94fc0b3cb2180daba71cd
jhmin99 <mjh012399@gmail.com>
2024-03-19 09:23:52 +0900

    style: Update Kor to Eng

--- src/main/java/jihong99/shoppingmall/dto/SignUpDto.java
+++ src/main/java/jihong99/shoppingmall/dto/SignUpDto.java
@@ -2,52 +2,61 @@ package jihong99.shoppingmall.dto;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import jihong99.shoppingmall.validation.groups.IdentificationValidation;
 import jihong99.shoppingmall.validation.groups.SignUpValidation;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-
 
 @Getter
 @AllArgsConstructor
 public class SignUpDto {
 
     /**
-     * 아이디
-     * regex :
-     * 영문, 숫자를 모두 포함하는 6-10자리 문자열
-     * 영문, 숫자로만 이루어짐
+     * ID
+     * regex:
+     * A 6-10 character string containing both alphabets and numbers
+     * Consists only of alphabets and numbers
      */
-    @NotNull(message = "아이디는 필수 항목입니다.", groups = {IdentificationValidation.class, SignUpValidation.class})
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)[a-zA-Z\\d]{6,10}$", message = "아이디는 영문, 숫자로만 이루어지며, 영문, 숫자를 모두 포함하는 6-10자리 문자열입니다.", groups = {IdentificationValidation.class, SignUpValidation.class})
+    @NotNull(message = "ID is a required field.", groups = {IdentificationValidation.class, SignUpValidation.class})
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)[a-zA-Z\\d]{6,10}$", message = "ID must consist of alphabets and numbers, containing both, with a length of 6-10 characters.", groups = {IdentificationValidation.class, SignUpValidation.class})
     private String identification;
 
     /**
-     * 비밀번호
-     * regex :
-     * 영문, 숫자, 특수문자를 모두 포함하는 8-15자리 문자열
-     * 특수문자는 !@#$%^&*만 유효
+     * Password
+     * regex:
+     * An 8-15 character string containing alphabets, numbers, and special characters
+     * Special characters allowed: !@#$%^&*
      */
-    @NotNull(message = "비밀번호는 필수 항목입니다.", groups = {SignUpValidation.class})
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*])[a-zA-Z\\d!@#$%^&*]{8,15}$", message = "비밀번호는 영문, 숫자, 특수문자를 모두 포함하는 8-15자리 문자열입니다. ( 특수문자는 !@#$%^&*만 유효 )", groups = {SignUpValidation.class})
+    @NotNull(message = "Password is a required field.", groups = {SignUpValidation.class})
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*])[a-zA-Z\\d!@#$%^&*]{8,15}$", message = "Password must contain alphabets, numbers, and special characters, with a length of 8-15 characters. (Allowed special characters: !@#$%^&*)", groups = {SignUpValidation.class})
     private String password;
 
-    // 비밀번호와 일치하는지 확인
-    @NotNull(message = "비밀번호 확인은 필수 항목입니다.", groups = {SignUpValidation.class})
+    /**
+     * Confirm Password
+     */
+    @NotNull(message = "Confirm Password is a required field.", groups = {SignUpValidation.class})
     private String confirmPassword;
 
-    // 이름
-    @NotNull(message = "이름은 필수 항목입니다.", groups = {SignUpValidation.class})
+    /**
+     * Name
+     */
+    @NotNull(message = "Name is a required field.", groups = {SignUpValidation.class})
     private String name;
 
-    // 생년월일
-    @NotNull(message = "생년월일은 필수 항목입니다.", groups = {SignUpValidation.class})
-    @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "생년월일은 xxxx-xx-xx 형식이어야 합니다.", groups = {SignUpValidation.class})
+    /**
+     * Birth Date
+     * Format: yyyy-mm-dd
+     */
+    @NotNull(message = "Birth Date is a required field.", groups = {SignUpValidation.class})
+    @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "Birth Date must be in the format yyyy-mm-dd.", groups = {SignUpValidation.class})
     private String birthDate;
 
-    // 핸드폰 번호
-    @NotNull(message = "핸드폰 번호는 필수 항목입니다.", groups = {SignUpValidation.class})
-    @Pattern(regexp = "\\d{11}", message = "핸드폰번호는 숫자로만 이루어진 11자리 문자열이어야 합니다.", groups = {SignUpValidation.class})
+    /**
+     * Phone Number
+     * regex:
+     * 11-digit string consisting only of numbers
+     */
+    @NotNull(message = "Phone Number is a required field.", groups = {SignUpValidation.class})
+    @Pattern(regexp = "\\d{11}", message = "Phone Number must consist of 11 digits.", groups = {SignUpValidation.class})
     private String phoneNumber;
 }
