0dc5078697c9063b77eb300f332d1610f79d3f99
jhmin99 <mjh012399@gmail.com>
2024-03-26 23:34:15 +0900

    feat: implement csrf config, AuthenticationManager Bean, change securityConfig, sessionManagement for signing in with session

--- src/main/java/jihong99/shoppingmall/config/auth/SecurityConfig.java
+++ src/main/java/jihong99/shoppingmall/config/auth/SecurityConfig.java
@@ -1,13 +1,21 @@
 package jihong99.shoppingmall.config.auth;
+import jihong99.shoppingmall.config.auth.filters.CustomCsrfCookieFilter;
+import jihong99.shoppingmall.config.auth.providers.CustomUsernamePwdAuthenticationProvider;
+import jihong99.shoppingmall.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -15,20 +23,27 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private CorsConfig config;
+    private CorsConfig corsConfig;
+    private UserRepository userRepository;
 
-    public SecurityConfig(CorsConfig config) {
-        this.config = config;
+    public SecurityConfig(CorsConfig corsConfig, UserRepository userRepository) {
+        this.corsConfig = corsConfig;
+        this.userRepository = userRepository;
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.
-                securityContext(
-                (context) -> context.requireExplicitSave(false))
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .csrf((csrfConfigurer -> csrfConfigurer.disable()))
-                .cors(corsCustomizer -> corsCustomizer.configurationSource(config.corsConfigurationSource()))
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName("_csrf");
+
+        http
+                .securityContext((context) -> context
+                        .requireExplicitSave(false))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .csrf(csrfConfigurer -> csrfConfigurer.csrfTokenRequestHandler(requestHandler).ignoringRequestMatchers("/api/users","/api/users/check-id","/api/login")
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                        .addFilterAfter(new CustomCsrfCookieFilter(), BasicAuthenticationFilter.class)
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(corsConfig.corsConfigurationSource()))
                 .authorizeHttpRequests(requests -> requests
                                 .requestMatchers(HttpMethod.GET, "/api/users/**").authenticated()
                                 .requestMatchers("/resources/**").denyAll()
@@ -44,4 +59,9 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(){
+        CustomUsernamePwdAuthenticationProvider authenticationProvider = new CustomUsernamePwdAuthenticationProvider(userRepository, passwordEncoder());
+        return new ProviderManager(authenticationProvider);
+    }
 }
