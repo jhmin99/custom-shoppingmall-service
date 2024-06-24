66c64eabe2deca103f5e69548fa70bca9bbfccff
jhmin99 <mjh012399@gmail.com>
2024-06-24 17:13:39 +0900

    refactor: Add @AllArgsConstructor to LoginDto for easier instantiation

--- src/main/java/jihong99/shoppingmall/dto/LoginDto.java
+++ src/main/java/jihong99/shoppingmall/dto/LoginDto.java
@@ -1,8 +1,10 @@
 package jihong99.shoppingmall.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class LoginDto {
     private String identification;
     private String password;
