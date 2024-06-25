86d22a9eb59e7f7ab8ec920225b62f6cd7677555
jhmin99 <mjh012399@gmail.com>
2024-06-25 18:04:21 +0900

    docs: Add Javadoc comments to JpaConfig for JPA Auditing

--- src/main/java/jihong99/shoppingmall/config/JpaConfig.java
+++ src/main/java/jihong99/shoppingmall/config/JpaConfig.java
@@ -3,6 +3,15 @@ package jihong99.shoppingmall.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+/**
+ * Configuration class for enabling JPA Auditing.
+ *
+ * <p>This configuration class enables JPA Auditing for the application.
+ * JPA Auditing allows for automatic population of auditing fields like
+ * created date, last modified date, created by, and last modified by
+ * in entity classes.</p>
+ */
 @Configuration
 @EnableJpaAuditing
 public class JpaConfig {
