bd97fea6f7ab7e6ea7079ce190ad3a296ae1364f
jhmin99 <mjh012399@gmail.com>
2024-06-25 18:14:21 +0900

    Enhance Roles enum to implement GrantedAuthority for Spring Security

--- src/main/java/jihong99/shoppingmall/entity/enums/Roles.java
+++ src/main/java/jihong99/shoppingmall/entity/enums/Roles.java
@@ -1,5 +1,26 @@
 package jihong99.shoppingmall.entity.enums;
 
-public enum Roles {
-    USER, ADMIN
+import org.springframework.security.core.GrantedAuthority;
+
+/**
+ * Enum representing the roles in the application.
+ *
+ * <p>This enum implements {@link GrantedAuthority} to be used by Spring Security for
+ * authorization purposes.</p>
+ */
+public enum Roles implements GrantedAuthority {
+    USER, ADMIN;
+
+    /**
+     * Returns the authority granted to the role.
+     *
+     * <p>This method is used by Spring Security to get the name of the role as a
+     * granted authority.</p>
+     *
+     * @return the name of the role
+     */
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }
