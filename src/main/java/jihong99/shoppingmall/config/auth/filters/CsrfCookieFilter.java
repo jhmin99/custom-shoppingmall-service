5a00daaa910b7f5ad56076f414e6aee752be966c
jhmin99 <mjh012399@gmail.com>
2024-06-25 17:50:58 +0900

    feat: Rename and enhance CsrfCookieFilter to CustomCsrfCookieFilter
    - Renamed CsrfCookieFilter to CustomCsrfCookieFilter for better clarity.
    - Annotated the filter with @Component for Spring to manage its lifecycle.
    - Added comprehensive Javadoc comments for better understanding of the filter's functionality.

--- src/main/java/jihong99/shoppingmall/config/auth/filters/CsrfCookieFilter.java
+++ src/main/java/jihong99/shoppingmall/config/auth/filters/CsrfCookieFilter.java
@@ -5,11 +5,24 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-
-public class CustomCsrfCookieFilter extends OncePerRequestFilter {
+/**
+ * This method is called for every request to ensure CSRF token is added to the response headers.
+ *
+ * <p>The filter retrieves the CSRF token from the request attributes and sets it as a response header.
+ * This is important for securing the application against Cross-Site Request Forgery (CSRF) attacks.</p>
+ *
+ * @param request  the HttpServletRequest object
+ * @param response the HttpServletResponse object
+ * @param filterChain the FilterChain object
+ * @throws ServletException if an error occurs during the servlet processing
+ * @throws IOException if an input or output error is detected
+ */
+@Component
+public class CsrfCookieFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         CsrfToken csrfToken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
