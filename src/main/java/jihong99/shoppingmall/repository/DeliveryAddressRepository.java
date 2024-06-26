8aaac5121faf0efe39af70e60dea21765c878db8
jhmin99 <mjh012399@gmail.com>
2024-06-26 18:52:16 +0900

    feat: Add DeliveryAddressRepository to handle delivery address operations

---
+++ src/main/java/jihong99/shoppingmall/repository/DeliveryAddressRepository.java
@@ -0,0 +1,10 @@
+package jihong99.shoppingmall.repository;
+
+import jihong99.shoppingmall.entity.DeliveryAddress;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Set;
+
+public interface DeliveryAddressRepository extends JpaRepository<DeliveryAddress, Long> {
+    Set<DeliveryAddress> findByUsersIdentification(String identification);
+}
