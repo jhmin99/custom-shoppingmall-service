8e98cf5b2badb4ce3a25230d09000f12f99b4cab
jhmin99 <mjh012399@gmail.com>
2024-06-28 15:15:42 +0900

    fix: update CsrfCookieFilter to set CSRF token in cookies

--- src/main/java/jihong99/shoppingmall/config/auth/filters/CsrfCookieFilter.java
+++ src/main/java/jihong99/shoppingmall/config/auth/filters/CsrfCookieFilter.java
@@ -2,6 +2,7 @@ package jihong99.shoppingmall.config.auth.filters;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.web.csrf.CsrfToken;
@@ -27,7 +28,9 @@ public class CsrfCookieFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         CsrfToken csrfToken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
         if(null != csrfToken.getHeaderName()){
-            response.setHeader(csrfToken.getHeaderName(), csrfToken.getToken());
+            Cookie cookie = new Cookie("XSRF-TOKEN", csrfToken.getToken());
+            cookie.setPath("/");
+            response.addCookie(cookie);
         }
         filterChain.doFilter(request, response);
     }
