fb66d0f0879d03bc0c00016ac0f0b1f777d1b61e
jhmin99 <mjh012399@gmail.com>
2024-06-28 15:23:09 +0900

    fix: change HTTP status for UserNotFoundException to 404 NOT FOUND fix: change HTTP status for UserNotFoundException to 404 NOT FOUND fix: change HTTP status for UserNotFoundException to 404 NOT FOUND

--- src/main/java/jihong99/shoppingmall/exception/UserNotFoundException.java
+++ src/main/java/jihong99/shoppingmall/exception/UserNotFoundException.java
@@ -3,7 +3,7 @@ package jihong99.shoppingmall.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException{
     public UserNotFoundException(String message){
         super(message);
