d49988d0c5585e5032358bc1884cd46e6592be70
jhmin99 <mjh012399@gmail.com>
2024-06-30 17:47:49 +0900

    feat: Create SuperAdminController for admin account creation

---
+++ src/main/java/jihong99/shoppingmall/controller/SuperAdminController.java
@@ -0,0 +1,54 @@
+package jihong99.shoppingmall.controller;
+
+import jihong99.shoppingmall.dto.ResponseDto;
+import jihong99.shoppingmall.dto.SignUpDto;
+import jihong99.shoppingmall.exception.DuplicateIdentificationException;
+import jihong99.shoppingmall.exception.PasswordMismatchException;
+import jihong99.shoppingmall.service.IUserService;
+import jihong99.shoppingmall.validation.groups.SignUpValidation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.format.DateTimeParseException;
+
+import static jihong99.shoppingmall.constants.UserConstants.*;
+import static jihong99.shoppingmall.constants.UserConstants.MESSAGE_400_WrongBirthDate;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/super-admin")
+@PreAuthorize("hasRole('SUPER_ADMIN')")
+public class SuperAdminController {
+
+    private final IUserService userService;
+
+    @PostMapping("/create-admin")
+    public ResponseEntity<ResponseDto> createAdmin(@RequestBody @Validated(SignUpValidation.class) SignUpDto adminRequest) {
+        try {
+            userService.signUpAdminAccount(adminRequest);
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .body(new ResponseDto(STATUS_201, MESSAGE_201_createUser));
+        } catch (DuplicateIdentificationException e) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(new ResponseDto(STATUS_400, MESSAGE_400_duplicatedId));
+        } catch (PasswordMismatchException e) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(new ResponseDto(STATUS_400, MESSAGE_400_MissMatchPw));
+        } catch (DateTimeParseException e) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(new ResponseDto(STATUS_400, MESSAGE_400_WrongBirthDate));
+        }
+    }
+}
+
