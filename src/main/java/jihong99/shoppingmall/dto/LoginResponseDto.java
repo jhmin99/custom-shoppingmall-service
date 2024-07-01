f4d88a975e7396c1ab9a89e7a1d83397f93d7875
jhmin99 <mjh012399@gmail.com>
2024-07-01 20:13:09 +0900

    refactor: Rename UserConstants to Constants

--- src/main/java/jihong99/shoppingmall/dto/LoginResponseDto.java
+++ src/main/java/jihong99/shoppingmall/dto/LoginResponseDto.java
@@ -1,6 +1,6 @@
 package jihong99.shoppingmall.dto;
 
-import jihong99.shoppingmall.constants.UserConstants;
+import jihong99.shoppingmall.constants.Constants;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -14,7 +14,7 @@ public class LoginResponseDto {
     private Long userId;
 
     public static LoginResponseDto success(String accessToken, String refreshToken, Long userId){
-        return  new LoginResponseDto(UserConstants.STATUS_200, UserConstants.MESSAGE_200_LoginSuccess, accessToken,refreshToken,userId);
+        return  new LoginResponseDto(Constants.STATUS_200, Constants.MESSAGE_200_LoginSuccess, accessToken,refreshToken,userId);
     }
 
     public static LoginResponseDto error(String statusCode, String statusMessage){
