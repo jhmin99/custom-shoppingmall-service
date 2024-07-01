de2a9bbcb8ffc4f99dc4191e9a8485f4169bba65
jhmin99 <mjh012399@gmail.com>
2024-07-01 20:12:08 +0900

    refactor: Add status code and message to PaginatedResponseDto

--- src/main/java/jihong99/shoppingmall/dto/PaginatedResponseDto.java
+++ src/main/java/jihong99/shoppingmall/dto/PaginatedResponseDto.java
@@ -9,13 +9,17 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 public class PaginatedResponseDto<T> {
+    private String statusCode;
+    private String statusMessage;
     private List<T> content;
     private int currentPage;
     private int totalPages;
     private long totalItems;
 
-    public static <T> PaginatedResponseDto<T> of(Page<T> page) {
+    public static <T> PaginatedResponseDto<T> of(Page<T> page, String statusCode, String statusMessage) {
         return new PaginatedResponseDto<>(
+                statusCode,
+                statusMessage,
                 page.getContent(),
                 page.getNumber(),
                 page.getTotalPages(),
