96ce49ecd8ad9f605e98bc6c9491aa0b496b5370
jhmin99 <mjh012399@gmail.com>
2024-07-01 20:18:46 +0900

    feat: Add CategoryRequestDto and CategoryResponseDto

---
+++ src/main/java/jihong99/shoppingmall/dto/CategoryRequestDto.java
@@ -0,0 +1,17 @@
+package jihong99.shoppingmall.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryRequestDto {
+
+    @NotNull(message = "name is a required field.")
+    @Size(min = 5, max = 20, message = "name must be between 5 and 20 characters.")
+    private String name;
+}
