bc09f835ad646f5ea12daa763c6a02c7da124641
jhmin99 <mjh012399@gmail.com>
2024-07-03 14:14:01 +0900

    refactor: Update SuperAdminController for improved exception handling and add detailed Javadoc

--- src/main/java/jihong99/shoppingmall/controller/SuperAdminController.java
+++ src/main/java/jihong99/shoppingmall/controller/SuperAdminController.java
@@ -11,15 +11,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.format.DateTimeParseException;
-
 import static jihong99.shoppingmall.constants.Constants.*;
-import static jihong99.shoppingmall.constants.Constants.MESSAGE_400_WrongBirthDate;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,27 +27,33 @@ import static jihong99.shoppingmall.constants.Constants.MESSAGE_400_WrongBirthDa
 public class SuperAdminController {
 
     private final IUserService iuserService;
-
+    /**
+     * Create a new admin account.
+     *
+     * <p>This endpoint allows a SUPER_ADMIN to create a new admin account.
+     * The request body must contain valid admin account details.</p>
+     *
+     * @param adminRequest DTO object containing necessary information for admin registration
+     * @return ResponseEntity<ResponseDto> Response object containing the result of the admin registration
+     * @success Admin account successfully created
+     * Response Code: 201
+     * @exception MethodArgumentNotValidException Validation failed (groups: {SignUpValidation.class})
+     * Response Code: 400
+     * @exception DuplicateIdentificationException Thrown if the identification already exists
+     * Response Code: 400
+     * @exception PasswordMismatchException Thrown if the passwords do not match
+     * Response Code: 400
+     * @exception DateTimeParseException Thrown if the birthdate is in an invalid format
+     * Response Code: 400
+     * @exception Exception Internal server error occurred
+     * Response Code: 500
+     */
     @PostMapping("/create-admin")
     public ResponseEntity<ResponseDto> createAdmin(@RequestBody @Validated(SignUpValidation.class) SignUpDto adminRequest) {
-        try {
-            iuserService.signUpAdminAccount(adminRequest);
-            return ResponseEntity
-                    .status(HttpStatus.CREATED)
-                    .body(new ResponseDto(STATUS_201, MESSAGE_201_createUser));
-        } catch (DuplicateIdentificationException e) {
-            return ResponseEntity
-                    .status(HttpStatus.BAD_REQUEST)
-                    .body(new ResponseDto(STATUS_400, MESSAGE_400_duplicatedId));
-        } catch (PasswordMismatchException e) {
-            return ResponseEntity
-                    .status(HttpStatus.BAD_REQUEST)
-                    .body(new ResponseDto(STATUS_400, MESSAGE_400_MissMatchPw));
-        } catch (DateTimeParseException e) {
-            return ResponseEntity
-                    .status(HttpStatus.BAD_REQUEST)
-                    .body(new ResponseDto(STATUS_400, MESSAGE_400_WrongBirthDate));
-        }
+        iuserService.signUpAdminAccount(adminRequest);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(STATUS_201, MESSAGE_201_createUser));
     }
 }
 
