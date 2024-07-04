341f934914ef71f173a9c919bb15457ee8cfae92
jhmin99 <mjh012399@gmail.com>
2024-07-04 23:13:48 +0900

    test: Add unit test for Mapper classes

---
+++ src/test/java/jihong99/shoppingmall/mapper/CategoryItemMapperTest.java
@@ -0,0 +1,26 @@
+package jihong99.shoppingmall.mapper;
+
+import jihong99.shoppingmall.entity.Category;
+import jihong99.shoppingmall.entity.CategoryItem;
+import jihong99.shoppingmall.entity.Item;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.*;
+
+class CategoryItemMapperTest {
+
+    @Test
+    void mapToCategoryItem() {
+        // given
+        Item item = new Item(null, "Sample Item", 100, 50, "#sample#sample2", LocalDate.now());
+        Category category = new Category(null, "Sample Category");
+        // when
+        CategoryItemMapper categoryItemMapper = new CategoryItemMapper();
+        CategoryItem categoryItem = categoryItemMapper.mapToCategoryItem(item,category);
+        // then
+        assertThat(categoryItem.getCategory()).isEqualTo(category);
+        assertThat(categoryItem.getItem()).isEqualTo(item);
+    }
+}
