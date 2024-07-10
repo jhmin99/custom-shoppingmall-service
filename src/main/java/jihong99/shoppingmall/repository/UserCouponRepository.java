c145f83d83aaa58ee011fe29d960fd96e2216123
jhmin99 <mjh012399@gmail.com>
2024-07-10 18:58:03 +0900

    feat: Enhance UserCouponRepository with additional query methods

--- src/main/java/jihong99/shoppingmall/repository/UserCouponRepository.java
+++ src/main/java/jihong99/shoppingmall/repository/UserCouponRepository.java
@@ -5,7 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 
 public interface UserCouponRepository extends JpaRepository<UserCoupon, Long> {
     Page<UserCoupon> findByUsersId(Long userId, Pageable pageable);
+    List<UserCoupon> findByUsersId(Long userId);
+    Optional<UserCoupon> findByUsersIdAndCouponId(Long userId, Long CouponId);
 }
