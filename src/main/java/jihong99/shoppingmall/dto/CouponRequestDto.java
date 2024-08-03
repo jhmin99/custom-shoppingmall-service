7290cd508dde735af67a8bd9a8bcbfb138e24f92
jhmin99 <mjh012399@gmail.com>
2024-08-03 18:13:44 +0900

    refactor: Update `CouponRequestDto` to include discount type, value, and expiration date fields.

--- src/main/java/jihong99/shoppingmall/dto/CouponRequestDto.java
+++ src/main/java/jihong99/shoppingmall/dto/CouponRequestDto.java
@@ -1,7 +1,10 @@
 package jihong99.shoppingmall.dto;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import jihong99.shoppingmall.entity.enums.DiscountType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,12 +12,18 @@ import java.time.LocalDate;
 @Getter
 @AllArgsConstructor
 public class CouponRequestDto {
-    @NotNull(message = "Name is a required field.")
-    @Size(min = 5, max = 20, message = "Name must be between 5 and 20 characters.")
-    private String name;
-    @NotNull(message = "Content is a required field.")
-    @Size(max = 100, message = "Content must be less than 100 characters.")
-    private String content;
-    @NotNull(message = "ExpirationDate is a required field.")
+    @NotNull(message = "Code is a required field.")
+    @Size(min = 3, max = 20, message = "Code must be between 3 and 20 characters.")
+    private String code;
+
+    @NotNull(message = "Discount type is a required field.")
+    private DiscountType discountType;
+
+    @NotNull(message = "Discount value is a required field.")
+    @Min(value = 0, message = "Discount value must be greater than or equal to 0.")
+    private Long discountValue;
+
+    @NotNull(message = "Expiration date is a required field.")
+    @Future(message = "Expiration date must be in the future.")
     private LocalDate expirationDate;
 }
