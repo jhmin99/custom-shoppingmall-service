06aec97d1057aec4f2140fa20ce1744d23f51d3d
jhmin99 <mjh012399@gmail.com>
2024-08-07 20:16:53 +0900

    feat: Add AdminSummaryResponseDto for summarizing admin details

---
+++ src/main/java/jihong99/shoppingmall/dto/response/AdminSummaryResponseDto.java
@@ -0,0 +1,23 @@
+package jihong99.shoppingmall.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+public class AdminSummaryResponseDto {
+    private Long id;
+    private String identification;
+    private String name;
+    private LocalDate birthDate;
+    private String phoneNumber;
+    private Timestamp creationTime;
+    private Timestamp lastModifiedTime;
+
+    public static AdminSummaryResponseDto of(Long id, String identification, String name, LocalDate birthDate, String phoneNumber, Timestamp creationTime, Timestamp lastModifiedTime){
+        return new AdminSummaryResponseDto(id, identification, name, birthDate, phoneNumber, creationTime, lastModifiedTime);
+    }
+}
