28b533b045ff25bfc4184768168b609f1ac6075f
jhmin99 <mjh012399@gmail.com>
2024-08-07 20:57:01 +0900

    feat: Add PatchUserRequestDto for user profile updates

---
+++ src/main/java/jihong99/shoppingmall/dto/request/PatchUserRequestDto.java
@@ -0,0 +1,20 @@
+package jihong99.shoppingmall.dto.request;
+
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PatchUserRequestDto {
+    @Size(min = 5, max = 20, message = "Name must be between 5 and 20 characters.")
+    private String name;
+
+    @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "Birth Date must be in the format yyyy-mm-dd.")
+    private String birthDate;
+
+    @Pattern(regexp = "\\d{11}", message = "Phone Number must consist of 11 digits.")
+    private String phoneNumber;
+
+}
