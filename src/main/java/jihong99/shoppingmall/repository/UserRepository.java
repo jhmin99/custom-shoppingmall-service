71b249f097b32866af8067b41d9bfc11e4ad5997
jhmin99 <mjh012399@gmail.com>
2024-08-07 21:19:00 +0900

    refactor: Remove Unnecessary query method for fetching user summaries, add findAllByRole method to streamline fetching users by role.

--- src/main/java/jihong99/shoppingmall/repository/UserRepository.java
+++ src/main/java/jihong99/shoppingmall/repository/UserRepository.java
@@ -1,26 +1,19 @@
 package jihong99.shoppingmall.repository;
 
-import jihong99.shoppingmall.dto.UserSummaryDto;
 import jihong99.shoppingmall.entity.Users;
-import jihong99.shoppingmall.entity.enums.Tiers;
+import jihong99.shoppingmall.entity.enums.Roles;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
     Optional<Users> findByIdentification(String identification);
-    @Query("SELECT new jihong99.shoppingmall.dto.UserSummaryDto(" +
-            "u.id, u.name, u.birthDate, da.id, da.zipCode, da.address, da.addressDetail, u.registrationDate) " +
-            "FROM Users u LEFT JOIN DeliveryAddress da ON da.users.id = u.id WHERE u.role = 'USER'")
-    Page<UserSummaryDto> findAllUserSummaries(Pageable pageable);
 
-    List<Users> findByTier(Tiers tier);
+    Page<Users> findAllByRole(Roles role, Pageable pageable);
 }
 
 
