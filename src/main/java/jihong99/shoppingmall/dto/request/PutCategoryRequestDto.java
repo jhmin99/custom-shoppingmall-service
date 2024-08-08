7b539ef14edc975d37f9929431cd7baf1a2932d0
jhmin99 <mjh012399@gmail.com>
2024-08-08 15:50:54 +0900

    feat: Add PutCategoryRequestDto for category update requests

---
+++ src/main/java/jihong99/shoppingmall/dto/request/PutCategoryRequestDto.java
@@ -0,0 +1,14 @@
+package jihong99.shoppingmall.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PutCategoryRequestDto {
+    @NotNull(message = "Name is a required field.")
+    @Size(min = 5, max = 20, message = "Name must be between 5 and 20 characters.")
+    private String name;
+}
