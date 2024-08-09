654556b4c16f00bdd62deed77493e6b8e40ca13b
jhmin99 <mjh012399@gmail.com>
2024-08-09 18:03:34 +0900

    refactor: Update IItemService interface, ItemServiceImpl to support image handling and stock management

--- src/main/java/jihong99/shoppingmall/service/IItemService.java
+++ src/main/java/jihong99/shoppingmall/service/IItemService.java
@@ -1,10 +1,16 @@
 package jihong99.shoppingmall.service;
 
-import jihong99.shoppingmall.dto.ItemRequestDto;
-import jihong99.shoppingmall.dto.ItemResponseDto;
+import jihong99.shoppingmall.dto.request.ItemRequestDto;
+import jihong99.shoppingmall.dto.request.PatchItemRequestDto;
+import jihong99.shoppingmall.dto.request.UpdateStockRequestDto;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface IItemService {
-    ItemResponseDto createItem(ItemRequestDto itemRequestDto);
-    void updateItem(Long itemId, ItemRequestDto itemRequestDto);
-    void deleteItem(Long itemId);
+    void createItem(ItemRequestDto itemRequestDto, List<MultipartFile> images);
+    void patchItem(Long id, PatchItemRequestDto patchItemRequestDto, List<MultipartFile> addImages, List<Long> removeImageIds);
+    void markItemAsInvalid(Long id);
+    void markItemAsValid(Long id);
+    void updateItemStock(Long id, UpdateStockRequestDto updateStockRequestDto);
 }
--- src/main/java/jihong99/shoppingmall/service/ItemServiceImpl.java
+++ src/main/java/jihong99/shoppingmall/service/ItemServiceImpl.java
@@ -1,20 +1,25 @@
 package jihong99.shoppingmall.service;
 
 import jakarta.transaction.Transactional;
-import jihong99.shoppingmall.dto.ItemRequestDto;
-import jihong99.shoppingmall.dto.ItemResponseDto;
+import jihong99.shoppingmall.dto.request.ItemRequestDto;
+import jihong99.shoppingmall.dto.request.PatchItemRequestDto;
+import jihong99.shoppingmall.dto.request.UpdateStockRequestDto;
 import jihong99.shoppingmall.entity.Category;
 import jihong99.shoppingmall.entity.CategoryItem;
+import jihong99.shoppingmall.entity.Image;
 import jihong99.shoppingmall.entity.Item;
+import jihong99.shoppingmall.exception.ImageUploadException;
 import jihong99.shoppingmall.exception.NotFoundException;
-import jihong99.shoppingmall.mapper.CategoryItemMapper;
-import jihong99.shoppingmall.mapper.ItemMapper;
 import jihong99.shoppingmall.repository.CategoryItemRepository;
 import jihong99.shoppingmall.repository.CategoryRepository;
+import jihong99.shoppingmall.repository.ImageRepository;
 import jihong99.shoppingmall.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,100 +27,262 @@ import static jihong99.shoppingmall.constants.Constants.*;
 
 @Service
 @RequiredArgsConstructor
-public class ItemServiceImpl implements IItemService{
+public class ItemServiceImpl implements IItemService {
     private final ItemRepository itemRepository;
     private final CategoryRepository categoryRepository;
     private final CategoryItemRepository categoryItemRepository;
+    private final ImageRepository imageRepository;
+    private final FileStorageService fileStorageService;
 
     /**
-     * Creates a new item and associates it with categories.
+     * Creates a new item with the provided details and images.
      *
-     * <p>This method maps the provided {@link ItemRequestDto} to an {@link Item} entity,
-     * saves the item in the database, and then associates the item with the specified categories.
-     * If a category ID provided in the request does not exist, a {@link NotFoundException} is thrown.</p>
-     *
-     * @param itemRequestDto the item request data transfer object containing the details of the item to be created
-     * @return an {@link ItemResponseDto} containing the details of the created item and its associated categories
-     * @throws NotFoundException if any of the specified category IDs do not exist
+     * @param itemRequestDto DTO containing item details such as name, price, stock, and categories
+     * @param images A list of images associated with the item
+     * @throws NotFoundException if any category ID provided does not exist
+     * @throws ImageUploadException if any image fails to upload
+     * @throws IllegalArgumentException if the storage type for file uploads is invalid
      */
     @Transactional
     @Override
-    public ItemResponseDto createItem(ItemRequestDto itemRequestDto) {
-        Item savedItem = createItemEntity(itemRequestDto);
-        saveCategoryItems(itemRequestDto, savedItem);
-        List<String> categoryNames = getCategoryNames(itemRequestDto);
-
-        return ItemResponseDto.of(savedItem.getId(), savedItem.getName(), savedItem.getPrice(), savedItem.getInventory(),
-                savedItem.getKeyword(), savedItem.getRegistrationDate(), categoryNames);
-    }
-    /**
-     * Updates an existing item with the specified details.
-     *
-     * <p>This method finds the item by its ID, updates its fields with the values from the provided {@link ItemRequestDto},
-     * and saves the updated item back to the database. If the item ID does not exist, a {@link NotFoundException} is thrown.</p>
-     *
-     * @param itemId the ID of the item to be updated
-     * @param itemRequestDto the item request data transfer object containing the updated item details
-     * @throws NotFoundException if the item with the specified ID does not exist
-     */
-    @Override
-    @Transactional
-    public void updateItem(Long itemId, ItemRequestDto itemRequestDto) {
-        Item item = itemRepository.findById(itemId).orElseThrow(() ->
-                new NotFoundException(MESSAGE_404_ItemNotFound));
-        applyItemUpdates(itemRequestDto, item);
-        itemRepository.save(item);
+    public void createItem(ItemRequestDto itemRequestDto, List<MultipartFile> images) {
+        validateCategoriesExist(itemRequestDto.getCategoryIds());
+        List<Image> imageEntities = uploadAndSaveImages(images);
+        Item item = buildItemFromRequest(itemRequestDto, imageEntities);
+        saveCategoryItems(itemRequestDto.getCategoryIds(), item);
     }
 
-    private void applyItemUpdates(ItemRequestDto itemRequestDto, Item item) {
-        item.updateName(itemRequestDto.getName());
-        item.updatePrice(itemRequestDto.getPrice());
-        item.updateInventory(itemRequestDto.getInventory());
-        item.updateKeyword(itemRequestDto.getKeyword());
-    }
     /**
-     * Deletes an existing item by its ID.
+     * Validates that all provided category IDs exist in the database.
      *
-     * <p>This method finds the item by its ID and deletes it from the database.
-     * If the item ID does not exist, a {@link NotFoundException} is thrown.</p>
-     *
-     * @param itemId the ID of the item to be deleted
-     * @throws NotFoundException if the item with the specified ID does not exist
+     * @param categoryIds List of category IDs to validate
+     * @throws NotFoundException if any category ID does not exist
      */
-    @Override
-    @Transactional
-    public void deleteItem(Long itemId) {
-        List<CategoryItem> categoryItems = categoryItemRepository.findByItemId(itemId);
-        if (!categoryItems.isEmpty()) {
-            categoryItemRepository.deleteAll(categoryItems);
+    private void validateCategoriesExist(List<Long> categoryIds) {
+        categoryIds.forEach(categoryId -> {
+            if (!categoryRepository.existsById(categoryId)) {
+                throw new NotFoundException(String.format(MESSAGE_404_CategoryNotFound, categoryId));  // Category not found
+            }
+        });
+    }
+
+    /**
+     * Uploads and saves images to the storage and returns their entity representations.
+     *
+     * @param images List of MultipartFile objects representing the images to be uploaded
+     * @return List of Image entities representing the uploaded images
+     * @throws ImageUploadException if any image fails to upload
+     */
+    private List<Image> uploadAndSaveImages(List<MultipartFile> images) {
+        if (images == null || images.isEmpty()) {
+            return new ArrayList<>();  // Return an empty list if no images are provided
         }
-
-        Item item = itemRepository.findById(itemId).orElseThrow(() ->
-                new NotFoundException(MESSAGE_404_ItemNotFound));
-        itemRepository.delete(item);
+        return images.stream()
+                .map(this::uploadAndSaveSingleImage)  // Upload and save each image
+                .collect(Collectors.toList());  // Collect the results into a list
     }
 
-    private Item createItemEntity(ItemRequestDto itemRequestDto) {
-        ItemMapper itemMapper = new ItemMapper();
-        Item item = itemMapper.mapToItem(itemRequestDto);
-        Item savedItem = itemRepository.save(item);
-        return savedItem;
+    /**
+     * Uploads and saves a single image to the storage and database.
+     *
+     * @param image MultipartFile representing the image to be uploaded
+     * @return Image entity representing the uploaded image
+     * @throws ImageUploadException if the image fails to upload
+     */
+    private Image uploadAndSaveSingleImage(MultipartFile image) {
+        try {
+            String imageUrl = fileStorageService.uploadFile(image.getOriginalFilename(), image.getInputStream(), image.getContentType());
+            Image savedImage = Image.builder()
+                    .name(image.getOriginalFilename())
+                    .url(imageUrl)
+                    .type(image.getContentType())
+                    .size(image.getSize())
+                    .build();
+            return imageRepository.save(savedImage);
+        } catch (IOException e) {
+            throw new ImageUploadException(MESSAGE_500_ImageUploadFailed);
+        }
     }
 
-    private void saveCategoryItems(ItemRequestDto itemRequestDto, Item savedItem) {
-        CategoryItemMapper categoryItemMapper = new CategoryItemMapper();
-        for (Long categoryId : itemRequestDto.getCategoryIds()) {
+    /**
+     * Builds an Item entity from the provided request data and image entities.
+     *
+     * @param itemRequestDto DTO containing item details such as name, price, stock, and keyword
+     * @param imageEntities List of Image entities to associate with the item
+     * @return The saved Item entity
+     */
+    private Item buildItemFromRequest(ItemRequestDto itemRequestDto, List<Image> imageEntities) {
+        Item item = Item.builder()
+                .name(itemRequestDto.getName())
+                .price(itemRequestDto.getPrice())
+                .stock(itemRequestDto.getStock())
+                .keyword(itemRequestDto.getKeyword())
+                .images(imageEntities)
+                .build();
+        return itemRepository.save(item);
+    }
+
+    /**
+     * Saves the associations between the created item and its categories.
+     *
+     * @param categoryIds The category IDs
+     * @param item The Item entity to be associated with the categories
+     */
+    private void saveCategoryItems(List<Long> categoryIds, Item item) {
+        for (Long categoryId : categoryIds) {
             Category category = categoryRepository.findById(categoryId)
                     .orElseThrow(() -> new NotFoundException(MESSAGE_404_CategoryNotFound));
-            CategoryItem categoryItem = categoryItemMapper.mapToCategoryItem(savedItem, category);
+            CategoryItem categoryItem = getCategoryItem(item, category);
             categoryItemRepository.save(categoryItem);
         }
     }
 
-    private List<String> getCategoryNames(ItemRequestDto itemRequestDto) {
-        return categoryRepository.findAllById(itemRequestDto.getCategoryIds())
-                .stream()
-                .map(Category::getName)
-                .collect(Collectors.toList());
+    /**
+     * Creates a CategoryItem entity representing the relationship between an item and a category.
+     *
+     * @param item The Item entity
+     * @param category The Category entity
+     * @return The created CategoryItem entity
+     */
+    private static CategoryItem getCategoryItem(Item item, Category category) {
+        CategoryItem categoryItem = CategoryItem.builder()
+                .category(category)
+                .item(item).build();
+        return categoryItem;
+    }
+
+    /**
+     * Updates an existing item with the provided details, images to add, and images to remove.
+     *
+     * @param itemId The ID of the item to update
+     * @param patchItemRequestDto DTO containing item details to update such as name, price, keyword, and categories
+     * @param addImages List of images to add to the item
+     * @param removeImageIds List of image IDs to remove from the item
+     * @throws NotFoundException if the item, category, or image does not exist
+     * @throws ImageUploadException if any image fails to upload
+     * @throws IllegalArgumentException if the storage type for file uploads is invalid
+     */
+    @Override
+    @Transactional
+    public void patchItem(Long itemId, PatchItemRequestDto patchItemRequestDto, List<MultipartFile> addImages, List<Long> removeImageIds) {
+        Item item = validateItemsExist(itemId);
+        if (patchItemRequestDto.getName() != null && !patchItemRequestDto.getName().isEmpty()) {
+            item.updateName(patchItemRequestDto.getName());
+        }
+        if (patchItemRequestDto.getPrice() != null) {
+            item.updatePrice(patchItemRequestDto.getPrice());
+        }
+        if (patchItemRequestDto.getKeyword() != null && !patchItemRequestDto.getKeyword().isEmpty()) {
+            item.updateKeyword(patchItemRequestDto.getKeyword());
+        }
+        if (patchItemRequestDto.getCategoryIds() != null && !patchItemRequestDto.getCategoryIds().isEmpty()) {
+            updateItemCategories(item, patchItemRequestDto.getCategoryIds());
+        }
+        if (removeImageIds != null && !removeImageIds.isEmpty()) {
+            removeImagesFromItem(item, removeImageIds);
+        }
+        if (addImages != null && !addImages.isEmpty()) {
+            List<Image> imageEntities = uploadAndSaveImages(addImages);
+            imageEntities.forEach(item::addImage);
+        }
+        itemRepository.save(item);
+    }
+
+    /**
+     * Marks an item as invalid, effectively making it unavailable for purchase.
+     *
+     * @param id The ID of the item to be marked as invalid
+     */
+    @Override
+    @Transactional
+    public void markItemAsInvalid(Long id) {
+        Item item = validateItemsExist(id);
+        item.invalidateItem();
+        itemRepository.save(item);
+    }
+    /**
+     * Marks an item as valid, making it available for purchase again.
+     *
+     * @param id The ID of the item to be marked as valid
+     */
+    @Override
+    @Transactional
+    public void markItemAsValid(Long id) {
+        Item item = validateItemsExist(id);
+        item.validateItem();
+        itemRepository.save(item);
+
+    }
+
+    /**
+     * Updates the stock levels of an item.
+     *
+     * @param id The ID of the item whose stock is to be updated
+     * @param updateStockRequestDto DTO containing the new stock level
+     */
+    @Override
+    @Transactional
+    public void updateItemStock(Long id, UpdateStockRequestDto updateStockRequestDto) {
+        Item item = validateItemsExist(id);
+        item.updateStock(updateStockRequestDto.getStock());
+        itemRepository.save(item);
+    }
+
+    /**
+     * Removes images from an item based on the provided list of image IDs.
+     *
+     * @param item The item from which images should be removed
+     * @param removeImageIds List of image IDs to remove from the item
+     * @throws NotFoundException if any of the images does not exist
+     */
+    private void removeImagesFromItem(Item item, List<Long> removeImageIds) {
+        removeImageIds.forEach(imageId -> {
+            Image image = validateImagesExist(imageId);
+            item.removeImage(image);
+            imageRepository.delete(image);
+        });
+    }
+
+    /**
+     * Validates that the image with the given ID exists in the database.
+     *
+     * @param imageId The ID of the image to validate
+     * @return The Image entity if found
+     * @throws NotFoundException if the image does not exist
+     */
+    private Image validateImagesExist(Long imageId) {
+        Image image = imageRepository.findById(imageId)
+                .orElseThrow(() -> new NotFoundException(MESSAGE_404_ImageNotFound));
+        return image;
+    }
+
+    /**
+     * Validates that the item with the given ID exists in the database.
+     *
+     * @param itemId The ID of the item to validate
+     * @return The Item entity if found
+     * @throws NotFoundException if the item does not exist
+     */
+    private Item validateItemsExist(Long itemId) {
+        return itemRepository.findById(itemId).orElseThrow(
+                () -> new NotFoundException(MESSAGE_404_ItemNotFound)
+        );
+    }
+
+    /**
+     * Updates the categories associated with the item by deleting existing associations
+     * and saving the new ones based on the provided category IDs.
+     *
+     * @param item The Item entity to update
+     * @param categoryIds List of category IDs to associate with the item
+     */
+    private void updateItemCategories(Item item, List<Long> categoryIds) {
+        categoryItemRepository.deleteAllByItem(item);
+        saveCategoryItems(categoryIds, item);
     }
 }
+
+
+
+
+
