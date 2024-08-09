0cd87ddb601707c23cacb67f5d6927bf7ad78f16
jhmin99 <mjh012399@gmail.com>
2024-08-09 18:16:13 +0900

    feat: Create ImageRepository interface for managing Image entities

---
+++ src/main/java/jihong99/shoppingmall/repository/ImageRepository.java
@@ -0,0 +1,7 @@
+package jihong99.shoppingmall.repository;
+
+import jihong99.shoppingmall.entity.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+}
