82e4282b250a10331036c34340833faf7906752a
jhmin99 <mjh012399@gmail.com>
2024-08-14 16:09:54 +0900

    refactor: Enhance ICouponService with new methods and DTO updates

--- src/main/java/jihong99/shoppingmall/service/ICouponService.java
+++ src/main/java/jihong99/shoppingmall/service/ICouponService.java
@@ -1,21 +1,24 @@
 package jihong99.shoppingmall.service;
 
 
-import jihong99.shoppingmall.dto.CouponRequestDto;
-import jihong99.shoppingmall.dto.CouponResponseDto;
-import jihong99.shoppingmall.entity.enums.Tiers;
+import jihong99.shoppingmall.dto.request.CouponRequestDto;
+import jihong99.shoppingmall.dto.request.PatchCouponRequestDto;
+import jihong99.shoppingmall.dto.response.CouponResponseDto;
+import jihong99.shoppingmall.dto.response.UserCouponDetailsResponseDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 
 public interface ICouponService {
-    Page<CouponResponseDto> getUserCoupons(Long id, Pageable pageable);
+    Page<UserCouponDetailsResponseDto> getUserCoupons(Long id, Pageable pageable);
 
-    CouponResponseDto createCoupon(CouponRequestDto couponRequestDto);
+    void createCoupon(CouponRequestDto couponRequestDto);
 
     Page<CouponResponseDto> getAllCoupons(Pageable pageable);
     void distributeCouponToUser(Long couponId, Long UserId);
-    void distributeCouponToUsersByTier(Long couponId, Tiers tier);
     void distributeCouponToAllUsers(Long couponId);
+
+    void deleteCoupon(Long couponId);
+    void patchCoupon(Long couponId, PatchCouponRequestDto patchCouponRequestDto);
 
 }
