71f9b1138f74ba53ba443daa2a31cb9d3f714571
jhmin99 <mjh012399@gmail.com>
2024-08-14 16:08:14 +0900

    refactor: Enhance CouponController with additional methods and comprehensive documentation

--- src/main/java/jihong99/shoppingmall/controller/CouponController.java
+++ src/main/java/jihong99/shoppingmall/controller/CouponController.java
@@ -1,17 +1,14 @@
 package jihong99.shoppingmall.controller;
 
 import jakarta.validation.Valid;
-import jihong99.shoppingmall.dto.CouponRequestDto;
-import jihong99.shoppingmall.dto.CouponResponseDto;
-import jihong99.shoppingmall.dto.PaginatedResponseDto;
-import jihong99.shoppingmall.dto.ResponseDto;
-import jihong99.shoppingmall.entity.enums.Tiers;
+import jihong99.shoppingmall.dto.request.CouponRequestDto;
+import jihong99.shoppingmall.dto.request.PatchCouponRequestDto;
+import jihong99.shoppingmall.dto.response.*;
 import jihong99.shoppingmall.exception.DuplicateNameException;
-import jihong99.shoppingmall.exception.InvalidExpirationDateException;
 import jihong99.shoppingmall.exception.NotFoundException;
 import jihong99.shoppingmall.service.ICouponService;
-import jihong99.shoppingmall.utils.annotation.HasId;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.TypeMismatchException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -22,8 +19,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
-import static jihong99.shoppingmall.constants.Constants.MESSAGE_200_DistributeCouponSuccess;
-import static jihong99.shoppingmall.constants.Constants.STATUS_200;
+import static jihong99.shoppingmall.constants.Constants.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -34,105 +30,109 @@ public class CouponController {
     /**
      * Creates a new coupon.
      *
-     * <p>This endpoint allows an admin user to create a new coupon. It validates the coupon details,
-     * checks for duplicate coupon names, and ensures the expiration date is valid.</p>
+     * <p>This endpoint allows an admin to create a new coupon. The request body must contain valid coupon details.</p>
      *
-     * @param couponRequestDto the data transfer object containing the coupon details
-     * @return the created coupon response
-     * @success Coupon object successfully created
+     * @param couponRequestDto DTO object containing necessary information for coupon creation
+     * @return ResponseEntity<ResponseDto> Response object containing the result of the coupon creation
+     * @success Coupon successfully created
      * Response Code: 201
-     * @exception MethodArgumentNotValidException Validation failed for the request body
+     * @throws MethodArgumentNotValidException Validation failed
      * Response Code: 400
-     * @exception DuplicateNameException Duplicate coupon name exists
+     * @throws DuplicateNameException Duplicate code exists
      * Response Code: 400
-     * @exception InvalidExpirationDateException Expiration date is in the past
-     * Response Code: 400
-     * @exception Exception Internal server error occurred
+     * @throws Exception Internal server error occurred
      * Response Code: 500
-     * @precondition The authenticated user must have the 'ADMIN' role
      */
-    @PostMapping("/admin/coupon")
+    @PostMapping("/admin/coupons")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<CouponResponseDto> createCoupon(@RequestBody @Valid CouponRequestDto couponRequestDto) {
-        CouponResponseDto couponResponseDto = icouponService.createCoupon(couponRequestDto);
+    public ResponseEntity<ResponseDto> createCoupon(@RequestBody @Valid CouponRequestDto couponRequestDto) {
+        icouponService.createCoupon(couponRequestDto);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(couponResponseDto);
+                .body(new ResponseDto(STATUS_201, MESSAGE_201_createCoupon));
     }
 
     /**
-     * Retrieves the user's coupons with pagination.
+     * Updates an existing coupon.
      *
-     * @param userId the user's id
-     * @param page the page number
-     * @param size the page size
-     * @return a paginated list of the user's coupons
-     * @success Valid response containing the paginated user coupons
+     * <p>This endpoint allows an admin to update an existing coupon. The coupon ID must be specified in the path,
+     * and the request body must contain valid coupon details.</p>
+     *
+     * @param couponId the ID of the coupon to be updated
+     * @param patchCouponRequestDto DTO object containing necessary information for coupon update
+     * @return ResponseEntity<ResponseDto> Response object containing the result of the coupon update
+     * @success Coupon successfully updated
      * Response Code: 200
-     * @exception NotFoundException Thrown if the user is not found
+     * @throws MethodArgumentNotValidException Validation failed
+     * Response Code: 400
+     * @throws TypeMismatchException Method argument (path variable or query parameter) cannot be converted to the expected type
+     * Response Code: 400
+     * @throws DuplicateNameException Duplicate code exists
+     * Response Code: 400
+     * @throws NotFoundException The coupon is not found
      * Response Code: 404
-     * @exception Exception Internal server error occurred
+     * @throws Exception Internal server error occurred
      * Response Code: 500
-     * @precondition The authenticated user must have the specified userId
      */
-    @HasId
-    @GetMapping("/users/coupons")
-    public ResponseEntity<PaginatedResponseDto<CouponResponseDto>> getUserCoupons(
-            @RequestParam Long userId,
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size
-    ) {
-        Pageable pageable = PageRequest.of(page, size);
-        Page<CouponResponseDto> userCoupons = icouponService.getUserCoupons(userId, pageable);
-        PaginatedResponseDto<CouponResponseDto> response = PaginatedResponseDto.of(userCoupons);
+    @PatchMapping("/admin/coupons/{couponId}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<ResponseDto> updateCoupon(
+            @RequestBody @Valid PatchCouponRequestDto patchCouponRequestDto,
+            @PathVariable Long couponId) {
+        icouponService.patchCoupon(couponId, patchCouponRequestDto);
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(response);
+                .body(new ResponseDto(STATUS_200, MESSAGE_200_UpdateCouponSuccess));
     }
 
     /**
-     * Retrieves all coupons with pagination.
+     * Deletes a coupon.
      *
-     * @param page the page number
-     * @param size the page size
-     * @return a paginated list of all coupons
-     * @success Valid response containing the paginated list of all coupons
+     * <p>This endpoint allows an admin to delete an existing coupon. The coupon ID must be specified in the path.</p>
+     *
+     * @param couponId the ID of the coupon to be deleted
+     * @return ResponseEntity<ResponseDto> Response object containing the result of the coupon deletion
+     * @success Coupon successfully deleted
      * Response Code: 200
-     * @exception Exception Internal server error occurred
+     * @throws TypeMismatchException Method argument (path variable or query parameter) cannot be converted to the expected type
+     * Response Code: 400
+     * @throws NotFoundException The coupon is not found
+     * Response Code: 404
+     * @throws Exception Internal server error occurred
      * Response Code: 500
-     * @precondition The authenticated user must have the 'ADMIN' role
      */
-    @GetMapping("/admin/coupons")
+    @DeleteMapping("/admin/coupons/{couponId}")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<PaginatedResponseDto<CouponResponseDto>> getAllCoupons(
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size
-    ) {
-        Pageable pageable = PageRequest.of(page, size);
-        Page<CouponResponseDto> allCoupons = icouponService.getAllCoupons(pageable);
-        PaginatedResponseDto<CouponResponseDto> response = PaginatedResponseDto.of(allCoupons);
+    public ResponseEntity<ResponseDto> deleteCoupon(@PathVariable Long couponId) {
+        icouponService.deleteCoupon(couponId);
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(response);
+                .body(new ResponseDto(STATUS_200, MESSAGE_200_DeleteCouponSuccess));
     }
 
     /**
-     * Distributes a coupon to a specific user.
+     * Assigns a coupon to a specific user.
      *
-     * @param couponId the ID of the coupon
-     * @param userId the ID of the user
-     * @return a response indicating the result of the distribution
-     * @success Coupon successfully distributed to the specified user
+     * <p>This endpoint allows an admin to assign a specific coupon to a specific user.
+     * The coupon ID and user ID must be specified in the path.</p>
+     *
+     * @param couponId the ID of the coupon to be assigned
+     * @param userId the ID of the user to whom the coupon will be assigned
+     * @return ResponseEntity<ResponseDto> Response object containing the result of the coupon assignment
+     * @success Coupon successfully assigned to user
      * Response Code: 200
-     * @exception NotFoundException Thrown if the user or coupon is not found
+     * @throws TypeMismatchException Method argument (path variable or query parameter) cannot be converted to the expected type
+     * Response Code: 400
+     * @throws NotFoundException Coupon or user not found
      * Response Code: 404
-     * @exception Exception Internal server error occurred
+     * @throws Exception Internal server error occurred
      * Response Code: 500
-     * @precondition The authenticated user must have the 'ADMIN' role
      */
-    @PostMapping("/admin/coupons/distribute/user")
+    @PostMapping("/admin/coupons/{couponId}/assign/{userId}")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<ResponseDto> distributeCouponToUser(@RequestParam Long couponId, @RequestParam Long userId) {
+    public ResponseEntity<ResponseDto> assignCouponToUser(
+            @PathVariable Long couponId,
+            @PathVariable Long userId) {
         icouponService.distributeCouponToUser(couponId, userId);
         return ResponseEntity
                 .status(HttpStatus.OK)
@@ -140,47 +140,88 @@ public class CouponController {
     }
 
     /**
-     * Distributes a coupon to users by tier.
+     * Assigns a coupon to all users.
      *
-     * @param couponId the ID of the coupon
-     * @param tier the tier of users
-     * @return a response indicating the result of the distribution
-     * @success Coupon successfully distributed to the specified tier of users
+     * <p>This endpoint allows an admin to assign a specific coupon to all users. The coupon ID must be specified in the path.</p>
+     *
+     * @param couponId the ID of the coupon to be assigned to all users
+     * @return ResponseEntity<ResponseDto> Response object containing the result of the coupon assignment
+     * @success Coupon successfully assigned to all users
      * Response Code: 200
-     * @exception NotFoundException Thrown if the user or coupon is not found
+     * @throws TypeMismatchException Method argument (path variable or query parameter) cannot be converted to the expected type
+     * Response Code: 400
+     * @throws NotFoundException The coupon is not found
      * Response Code: 404
-     * @exception Exception Internal server error occurred
+     * @throws Exception Internal server error occurred
      * Response Code: 500
-     * @precondition The authenticated user must have the 'ADMIN' role
      */
-    @PostMapping("/admin/coupons/distribute/tier")
+    @PostMapping("/admin/coupons/{couponId}/assign/all")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<ResponseDto> distributeCouponToUsersByTier(@RequestParam Long couponId, @RequestParam Tiers tier) {
-        icouponService.distributeCouponToUsersByTier(couponId, tier);
+    public ResponseEntity<ResponseDto> assignCouponToAll(
+            @PathVariable Long couponId) {
+        icouponService.distributeCouponToAllUsers(couponId);
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(new ResponseDto(STATUS_200, MESSAGE_200_DistributeCouponSuccess));
     }
 
     /**
-     * Distributes a coupon to all users.
+     * Retrieves all coupons.
      *
-     * @param couponId the ID of the coupon
-     * @return a response indicating the result of the distribution
-     * @success Coupon successfully distributed to all users
+     * <p>This endpoint allows an admin to retrieve a paginated list of all coupons.</p>
+     *
+     * @param page the page number to retrieve (for pagination)
+     * @param size the number of coupons to retrieve per page (maximum 10)
+     * @return ResponseEntity<PaginatedResponseDto<CouponResponseDto>> a paginated response containing the coupons
+     * @success Valid response containing the paginated coupons
      * Response Code: 200
-     * @exception NotFoundException Thrown if the user or coupon is not found
-     * Response Code: 404
-     * @exception Exception Internal server error occurred
+     * @throws TypeMismatchException Method argument (path variable or query parameter) cannot be converted to the expected type
+     * Response Code: 400
+     * @throws Exception Internal server error occurred
      * Response Code: 500
-     * @precondition The authenticated user must have the 'ADMIN' role
      */
-    @PostMapping("/admin/coupons/distribute/all")
+    @GetMapping("/admin/coupons")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<ResponseDto> distributeCouponToAllUsers(@RequestParam Long couponId) {
-        icouponService.distributeCouponToAllUsers(couponId);
+    public ResponseEntity<PaginatedResponseDto<CouponResponseDto>> getAllCoupons(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<CouponResponseDto> coupons = icouponService.getAllCoupons(pageable);
+        PaginatedResponseDto<CouponResponseDto> response = PaginatedResponseDto.of(coupons);
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(new ResponseDto(STATUS_200, MESSAGE_200_DistributeCouponSuccess));
+                .body(response);
+    }
+
+    /**
+     * Retrieves all coupons for a specific user.
+     *
+     * <p>This endpoint allows an admin to retrieve a paginated list of all coupons assigned to a specific user.</p>
+     *
+     * @param userId the ID of the user whose coupons are being requested
+     * @param page the page number to retrieve (for pagination)
+     * @param size the number of coupons to retrieve per page (maximum 10)
+     * @return ResponseEntity<PaginatedResponseDto<UserCouponDetailsResponseDto>> a paginated response containing the user's coupons
+     * @success Valid response containing the paginated user coupons
+     * Response Code: 200
+     * @throws TypeMismatchException Method argument (path variable or query parameter) cannot be converted to the expected type
+     * Response Code: 400
+     * @throws NotFoundException The user is not found
+     * Response Code: 404
+     * @throws Exception Internal server error occurred
+     * Response Code: 500
+     */
+    @GetMapping("/admin/coupons/users/{userId}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<PaginatedResponseDto<UserCouponDetailsResponseDto>> getAllUserCoupons(
+            @PathVariable Long userId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<UserCouponDetailsResponseDto> coupons = icouponService.getUserCoupons(userId, pageable);
+        PaginatedResponseDto<UserCouponDetailsResponseDto> response = PaginatedResponseDto.of(coupons);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(response);
     }
 }
