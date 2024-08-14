5894612f05d40b459f573e0bfa6d4f0044461cd8
jhmin99 <mjh012399@gmail.com>
2024-08-14 18:25:52 +0900

    refactor: Simplify UserCouponRepository by removing unused methods

--- src/main/java/jihong99/shoppingmall/repository/UserCouponRepository.java
+++ src/main/java/jihong99/shoppingmall/repository/UserCouponRepository.java
@@ -1,16 +1,10 @@
 package jihong99.shoppingmall.repository;
 
 import jihong99.shoppingmall.entity.UserCoupon;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
 
 
 public interface UserCouponRepository extends JpaRepository<UserCoupon, Long> {
-    Page<UserCoupon> findByUsersId(Long userId, Pageable pageable);
-    List<UserCoupon> findByUsersId(Long userId);
-    Optional<UserCoupon> findByUsersIdAndCouponId(Long userId, Long CouponId);
+    void deleteAllByCouponId(Long couponId);
 }
