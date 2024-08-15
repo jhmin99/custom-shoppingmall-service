8754e6db7ad333b87389a20dc9c0cc88cf2402f2
jhmin99 <mjh012399@gmail.com>
2024-08-15 23:25:50 +0900

    feat: Add custom queries to UserRepository
    - Added method `findAllByRole` to retrieve users by role as a list.
    - Implemented custom query `findUsersByCartIds` to find users based on cart IDs.

--- src/main/java/jihong99/shoppingmall/repository/UserRepository.java
+++ src/main/java/jihong99/shoppingmall/repository/UserRepository.java
@@ -5,8 +5,11 @@ import jihong99.shoppingmall.entity.enums.Roles;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,6 +17,11 @@ public interface UserRepository extends JpaRepository<Users, Long> {
     Optional<Users> findByIdentification(String identification);
 
     Page<Users> findAllByRole(Roles role, Pageable pageable);
+
+    List<Users> findAllByRole(Roles role);
+    @Query("SELECT u FROM Users u WHERE u.cart.id IN :cartIds")
+    List<Users> findUsersByCartIds(@Param("cartIds") List<Long> cartIds);
+
 }
 
 
