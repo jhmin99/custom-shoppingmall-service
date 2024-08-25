b686d546b2d6f4b7b8efe8fd4c2afa57429e35a8
jhmin99 <mjh012399@gmail.com>
2024-08-25 17:26:10 +0900

    refactor: standardize endpoint paths and enhance response handling

--- src/main/java/jihong99/shoppingmall/controller/InquiryController.java
+++ src/main/java/jihong99/shoppingmall/controller/InquiryController.java
@@ -1,14 +1,23 @@
 package jihong99.shoppingmall.controller;
 
 import jakarta.validation.Valid;
+import jihong99.shoppingmall.dto.request.inquiry.ChildResponseRequestDto;
 import jihong99.shoppingmall.dto.request.inquiry.PatchResponseRequestDto;
 import jihong99.shoppingmall.dto.request.inquiry.ResponseRequestDto;
+import jihong99.shoppingmall.dto.response.inquiry.InquiryDetailsResponseDto;
+import jihong99.shoppingmall.dto.response.inquiry.InquiryResponseDto;
+import jihong99.shoppingmall.dto.response.shared.PaginatedResponseDto;
 import jihong99.shoppingmall.dto.response.shared.ResponseDto;
+import jihong99.shoppingmall.entity.enums.InquiryStatus;
+import jihong99.shoppingmall.entity.enums.InquiryType;
 import jihong99.shoppingmall.exception.HasRelatedEntitiesException;
 import jihong99.shoppingmall.exception.NotFoundException;
 import jihong99.shoppingmall.service.IInquiryService;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.TypeMismatchException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -27,13 +36,82 @@ public class InquiryController {
     private final IInquiryService iinquiryService;
 
     /**
-     * Responds to an inquiry.
+     * Retrieves all inquiries based on type, status, and pagination parameters.
      *
-     * <p>This endpoint allows an admin to respond to a specific inquiry. The inquiry ID must be provided
-     * as a path variable, and the response details are provided in the request body.</p>
+     * @param type   The type of the inquiry (item, customer)
+     * @param status The status of the inquiry (resolved, unresolved), optional
+     * @param page   The page number to retrieve, starting from 0
+     * @param size   The number of items per page
+     * @return A ResponseEntity containing a paginated list of inquiries
      *
-     * @param inquiryId The ID of the inquiry to respond to
-     * @param responseRequestDto DTO containing response details such as content
+     * @success Response successfully retrieved
+     * Response Code: 200
+     *
+     * @throws TypeMismatchException if the method argument cannot be converted to the expected type
+     * Response Code: 400
+     * @throws AccessDeniedException if the user does not have the 'ADMIN' role
+     * Response Code: 403
+     *
+     * @throws Exception if any other internal server error occurs
+     * Response Code: 500
+     */
+    @GetMapping("/admin/inquiries")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<PaginatedResponseDto<InquiryResponseDto>> getAllInquiries(
+            @RequestParam InquiryType type,
+            @RequestParam(required = false) InquiryStatus status,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        Pageable pageable = PageRequest.of(page, size);
+
+        Page<InquiryResponseDto> inquiries = iinquiryService.getAllInquiries(type, status, pageable);
+        PaginatedResponseDto<InquiryResponseDto> response = PaginatedResponseDto.of(inquiries);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(response);
+    }
+
+    /**
+     * Retrieves the details of a specific inquiry by its ID.
+     *
+     * @param inquiryId The ID of the inquiry
+     * @return A ResponseEntity containing the details of the inquiry
+     *
+     * @success Inquiry details successfully retrieved
+     * Response Code: 200
+     *
+     * @throws TypeMismatchException if the method argument (inquiryId) cannot be converted to the expected type
+     * Response Code: 400
+     *
+     * @throws AccessDeniedException if the user does not have the 'ADMIN' role
+     * Response Code: 403
+     *
+     * @throws NotFoundException if the inquiry is not found
+     * Response Code: 404
+     *
+     * @throws Exception if any other internal server error occurs
+     * Response Code: 500
+     */
+    @GetMapping("/admin/inquiries/{inquiryId}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<InquiryDetailsResponseDto> getInquiryDetails(
+            @PathVariable Long inquiryId
+    ) {
+        InquiryDetailsResponseDto inquiryDetails = iinquiryService.getInquiryDetails(inquiryId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(inquiryDetails);
+    }
+
+    /**
+     * Responds to a specific inquiry.
+     *
+     * <p>This endpoint allows an admin to respond to an inquiry with a given inquiry ID. The response details
+     * are provided in the request body.</p>
+     *
+     * @param inquiryId           The ID of the inquiry to respond to
+     * @param responseRequestDto  DTO containing response details such as content
      * @return A ResponseEntity containing the status of the response creation operation
      *
      * @success Response successfully created
@@ -54,7 +132,7 @@ public class InquiryController {
      * @throws Exception if any other internal server error occurs
      * Response Code: 500
      */
-    @PostMapping("/admin/inquiries/{inquiryId}/respond")
+    @PostMapping("/admin/inquiries/{inquiryId}/responds")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ResponseDto> respondToInquiry(
             @PathVariable Long inquiryId,
@@ -72,9 +150,9 @@ public class InquiryController {
      * <p>This endpoint allows an admin to edit a specific response associated with an inquiry. The inquiry ID and
      * response ID must be provided as path variables, and the updated response details are provided in the request body.</p>
      *
-     * @param inquiryId The ID of the inquiry
-     * @param respondId The ID of the response to be edited
-     * @param patchRepondRequestDto DTO containing updated response details
+     * @param inquiryId              The ID of the inquiry
+     * @param responseId             The ID of the response to be edited
+     * @param patchRespondRequestDto DTO containing updated response details
      * @return A ResponseEntity containing the status of the response update operation
      *
      * @success Response successfully updated
@@ -83,7 +161,7 @@ public class InquiryController {
      * @throws MethodArgumentNotValidException if the request data is invalid (e.g., missing required fields)
      * Response Code: 400
      *
-     * @throws TypeMismatchException if the method argument (inquiryId or respondId) cannot be converted to the expected type
+     * @throws TypeMismatchException if the method argument (inquiryId or responseId) cannot be converted to the expected type
      * Response Code: 400
      *
      * @throws AccessDeniedException if the user does not have the 'ADMIN' role
@@ -95,14 +173,14 @@ public class InquiryController {
      * @throws Exception if any other internal server error occurs
      * Response Code: 500
      */
-    @PatchMapping("/admin/inquiries/{inquiryId}/respond/{respondId}")
+    @PatchMapping("/admin/inquiries/{inquiryId}/responses/{responseId}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ResponseDto> editInquiryResponse(
             @PathVariable Long inquiryId,
-            @PathVariable Long respondId,
-            @Valid @RequestBody PatchResponseRequestDto patchRepondRequestDto
+            @PathVariable Long responseId,
+            @Valid @RequestBody PatchResponseRequestDto patchRespondRequestDto
     ) {
-        iinquiryService.editInquiryResponse(inquiryId, respondId, patchRepondRequestDto);
+        iinquiryService.editInquiryResponse(inquiryId, responseId, patchRespondRequestDto);
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(new ResponseDto(STATUS_200, MESSAGE_200_UpdateRespondSuccess));
@@ -115,13 +193,13 @@ public class InquiryController {
      * response ID are provided as path variables.</p>
      *
      * @param inquiryId The ID of the inquiry
-     * @param respondId The ID of the response to be deleted
+     * @param responseId The ID of the response to be deleted
      * @return A ResponseEntity containing the status of the deletion operation
      *
      * @success Response successfully deleted
      * Response Code: 200
      *
-     * @throws TypeMismatchException if the method argument (inquiryId or respondId) cannot be converted to the expected type
+     * @throws TypeMismatchException if the method argument (inquiryId or responseId) cannot be converted to the expected type
      * Response Code: 400
      *
      * @throws AccessDeniedException if the user does not have the 'ADMIN' role
@@ -136,29 +214,34 @@ public class InquiryController {
      * @throws Exception if any other internal server error occurs
      * Response Code: 500
      */
-    @DeleteMapping("/admin/inquiries/{inquiryId}/respond/{respondId}")
+    @DeleteMapping("/admin/inquiries/{inquiryId}/responses/{responseId}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ResponseDto> deleteInquiryResponse(
             @PathVariable Long inquiryId,
-            @PathVariable Long respondId
+            @PathVariable Long responseId
     ) {
-        iinquiryService.deleteInquiryResponse(inquiryId, respondId);
+        iinquiryService.deleteInquiryResponse(inquiryId, responseId);
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(new ResponseDto(STATUS_200, MESSAGE_200_DeleteRespondSuccess));
     }
 
     /**
-     * Updates the status of an inquiry to resolved or unresolved.
+     * Responds to a parent response within an inquiry.
      *
-     * <p>This endpoint allows an admin to update the status of a specific inquiry. The inquiry ID must be provided
-     * as a path variable.</p>
+     * <p>This endpoint allows an admin to respond to a parent response within an inquiry. The inquiry ID and
+     * response details are provided as path variables.</p>
      *
-     * @param inquiryId The ID of the inquiry
-     * @return A ResponseEntity containing the status of the inquiry update operation
+     * @param inquiryId           The ID of the inquiry
+     * @param responseId           The ID of the response
+     * @param responseRequestDto  DTO containing response details such as content
+     * @return A ResponseEntity containing the status of the response creation operation
      *
-     * @success Inquiry status successfully updated
-     * Response Code: 200
+     * @success Response successfully created
+     * Response Code: 201
+     *
+     * @throws MethodArgumentNotValidException if the request data is invalid (e.g., missing required fields)
+     * Response Code: 400
      *
      * @throws TypeMismatchException if the method argument (inquiryId) cannot be converted to the expected type
      * Response Code: 400
@@ -166,21 +249,22 @@ public class InquiryController {
      * @throws AccessDeniedException if the user does not have the 'ADMIN' role
      * Response Code: 403
      *
-     * @throws NotFoundException if the inquiry is not found
+     * @throws NotFoundException if the parent inquiry or response is not found
      * Response Code: 404
      *
      * @throws Exception if any other internal server error occurs
      * Response Code: 500
      */
-    @PatchMapping("/admin/inquiries/{inquiryId}")
+    @PostMapping("/admin/inquiries/{inquiryId}/responses/{responseId}")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<ResponseDto> updateInquiryStatus(
-            @PathVariable Long inquiryId
+    public ResponseEntity<ResponseDto> respondToParentResponse(
+            @PathVariable Long inquiryId,
+            @PathVariable Long responseId,
+            @RequestBody @Valid ChildResponseRequestDto responseRequestDto
     ) {
-        iinquiryService.updateInquiryStatus(inquiryId);
+        iinquiryService.respondToParentResponse(inquiryId, responseId, responseRequestDto);
         return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(new ResponseDto(STATUS_200, MESSAGE_200_UpdateInquiryStatusSuccess));
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(STATUS_201, MESSAGE_201_createRespondSuccess));
     }
-
 }
