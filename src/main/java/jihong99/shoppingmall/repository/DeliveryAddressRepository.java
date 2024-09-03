bcb848544c8eb499695f5a7a7b962b6177bcd513
jhmin99 <mjh012399@gmail.com>
2024-09-03 12:15:55 +0900

    refactor: Rename method to follow naming conventions in DeliveryAddressRepository

--- src/main/java/jihong99/shoppingmall/repository/DeliveryAddressRepository.java
+++ src/main/java/jihong99/shoppingmall/repository/DeliveryAddressRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Set;
 
 public interface DeliveryAddressRepository extends JpaRepository<DeliveryAddress, Long> {
-    Set<DeliveryAddress> findByUsersId(Long id);
+    Set<DeliveryAddress> findAllByUsersId(Long id);
 
 }
