e5caa9ea09f77720f9ba263ea52df598ba3711b4
jhmin99 <mjh012399@gmail.com>
2024-09-03 12:15:14 +0900

    refactor: Modify DeliveryAddressController to handle user-specific operations

--- src/main/java/jihong99/shoppingmall/controller/DeliveryAddressController.java
+++ src/main/java/jihong99/shoppingmall/controller/DeliveryAddressController.java
@@ -5,10 +5,14 @@ import jihong99.shoppingmall.dto.request.deliveryAddress.DeliveryAddressRequestD
 import jihong99.shoppingmall.dto.response.shared.ResponseDto;
 import jihong99.shoppingmall.exception.NotFoundException;
 import jihong99.shoppingmall.service.IDeliveryAddressService;
+import jihong99.shoppingmall.utils.annotation.HasId;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.TypeMismatchException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import static jihong99.shoppingmall.constants.Constants.*;
@@ -20,73 +24,109 @@ public class DeliveryAddressController {
     private final IDeliveryAddressService ideliveryAddressService;
 
     /**
-     * Adds a new delivery address for a user.
+     * Adds a new delivery address for the specified user.
      *
-     * <p>This endpoint is used to add a new delivery address for a user.</p>
+     * @param userId The ID of the user to whom the address will be added.
+     * @param deliveryAddressRequestDto The DTO containing the address details.
+     * @return A ResponseEntity with status 201 (Created) and a message indicating successful creation.
      *
-     * @param requestDto DTO object containing the delivery address information
-     * @return ResponseEntity<ResponseDto> Response object containing the result of the add operation
-     * @success Valid response indicating that the delivery address was successfully added
+     * @success Response successfully created.
      * Response Code: 201
-     * @exception NotFoundException Thrown if the user with the given ID is not found
+     *
+     * @throws MethodArgumentNotValidException if the request data is invalid (e.g., missing required fields).
+     * Response Code: 400
+     *
+     * @throws TypeMismatchException if the method argument (userId) cannot be converted to the expected type.
+     * Response Code: 400
+     *
+     * @throws NotFoundException if the user with the specified ID is not found.
      * Response Code: 404
-     * @exception Exception Internal server error occurred
+     *
+     * @throws AccessDeniedException if the user does not have the required role.
+     * Response Code: 403
+     *
+     * @throws Exception if any other internal server error occurs.
      * Response Code: 500
      */
-    @PostMapping("/delivery-address")
-    public ResponseEntity<ResponseDto> addDeliveryAddress(@Valid @RequestBody DeliveryAddressRequestDto requestDto) {
-        ideliveryAddressService.addDeliveryAddress(requestDto);
+    @HasId
+    @PostMapping("/{userId}/deliveryAddresses")
+    public ResponseEntity<ResponseDto> addDeliveryAddress(
+            @PathVariable Long userId,
+            @Valid @RequestBody DeliveryAddressRequestDto deliveryAddressRequestDto) {
+        ideliveryAddressService.addDeliveryAddress(userId, deliveryAddressRequestDto);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(new ResponseDto(STATUS_201, MESSAGE_201_createDeliveryAddress));
-
     }
 
     /**
-     * Updates an existing delivery address for a user.
+     * Updates an existing delivery address for the specified user.
      *
-     * <p>This endpoint is used to update an existing delivery address for a user.</p>
+     * @param userId The ID of the user.
+     * @param deliveryAddressId The ID of the delivery address to update.
+     * @param deliveryAddressRequestDto The DTO containing the updated address details.
+     * @return A ResponseEntity with status 200 (OK) and a message indicating successful update.
      *
-     * @param addressId The ID of the delivery address to be updated
-     * @param requestDto DTO object containing the updated delivery address information
-     * @return ResponseEntity<ResponseDto> Response object containing the result of the update operation
-     * @success Valid response indicating that the delivery address was successfully updated
+     * @success Response successfully updated.
      * Response Code: 200
-     * @exception NotFoundException Thrown if the delivery address with the given ID is not found
+     *
+     * @throws MethodArgumentNotValidException if the request data is invalid (e.g., missing required fields).
+     * Response Code: 400
+     *
+     * @throws TypeMismatchException if the method argument (userId or deliveryAddressId) cannot be converted to the expected type.
+     * Response Code: 400
+     *
+     * @throws NotFoundException if the user or delivery address with the specified ID is not found.
      * Response Code: 404
-     * @exception Exception Internal server error occurred
+     *
+     * @throws AccessDeniedException if the user does not have the required role.
+     * Response Code: 403
+     *
+     * @throws Exception if any other internal server error occurs.
      * Response Code: 500
      */
-    @PutMapping("/delivery-address/{addressId}")
-    public ResponseEntity<ResponseDto> updateDeliveryAddress(@PathVariable Long addressId, @Valid @RequestBody DeliveryAddressRequestDto requestDto) {
-        ideliveryAddressService.updateDeliveryAddress(addressId, requestDto);
+    @HasId
+    @PutMapping("/{userId}/deliveryAddresses/{deliveryAddressId}")
+    public ResponseEntity<ResponseDto> updateDeliveryAddress(
+            @PathVariable Long userId,
+            @PathVariable Long deliveryAddressId,
+            @Valid @RequestBody DeliveryAddressRequestDto deliveryAddressRequestDto) {
+        ideliveryAddressService.updateDeliveryAddress(userId, deliveryAddressId, deliveryAddressRequestDto);
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(new ResponseDto(STATUS_200, MESSAGE_200_UpdateDeliveryAddressSuccess));
-
     }
 
     /**
-     * Deletes an existing delivery address for a user.
+     * Deletes an existing delivery address for the specified user.
      *
-     * <p>This endpoint is used to delete an existing delivery address for a user.</p>
+     * @param userId The ID of the user.
+     * @param deliveryAddressId The ID of the delivery address to delete.
+     * @return A ResponseEntity with status 200 (OK) and a message indicating successful deletion.
      *
-     * @param addressId The ID of the delivery address to be deleted
-     * @return ResponseEntity<ResponseDto> Response object containing the result of the delete operation
-     * @success Valid response indicating that the delivery address was successfully deleted
+     * @success Response successfully deleted.
      * Response Code: 200
-     * @exception NotFoundException Thrown if the delivery address with the given ID is not found
+     *
+     * @throws TypeMismatchException if the method argument (userId or deliveryAddressId) cannot be converted to the expected type.
+     * Response Code: 400
+     *
+     * @throws NotFoundException if the user or delivery address with the specified ID is not found.
      * Response Code: 404
-     * @exception Exception Internal server error occurred
+     *
+     * @throws AccessDeniedException if the user does not have the required role.
+     * Response Code: 403
+     *
+     * @throws Exception if any other internal server error occurs.
      * Response Code: 500
      */
-    @DeleteMapping("/delivery-address/{addressId}")
-    public ResponseEntity<ResponseDto> deleteDeliveryAddress(@PathVariable Long addressId) {
-        ideliveryAddressService.deleteDeliveryAddress(addressId);
+    @HasId
+    @DeleteMapping("/{userId}/deliveryAddresses/{deliveryAddressId}")
+    public ResponseEntity<ResponseDto> deleteDeliveryAddress(
+            @PathVariable Long userId,
+            @PathVariable Long deliveryAddressId) {
+        ideliveryAddressService.deleteDeliveryAddress(userId, deliveryAddressId);
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(new ResponseDto(STATUS_200, MESSAGE_200_DeleteDeliveryAddressSuccess));
-
     }
-
 }
