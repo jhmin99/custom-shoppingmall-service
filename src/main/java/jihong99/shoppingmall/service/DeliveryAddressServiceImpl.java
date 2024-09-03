0c8fe182540213003570dffd833a09a5d077d954
jhmin99 <mjh012399@gmail.com>
2024-09-03 12:11:22 +0900

    refactor: Simplify DeliveryAddressServiceImpl and improve method clarity

--- src/main/java/jihong99/shoppingmall/service/DeliveryAddressServiceImpl.java
+++ src/main/java/jihong99/shoppingmall/service/DeliveryAddressServiceImpl.java
@@ -1,15 +1,14 @@
 package jihong99.shoppingmall.service;
 
-import jakarta.transaction.Transactional;
-import jihong99.shoppingmall.dto.DeliveryAddressDto;
+import jihong99.shoppingmall.dto.request.deliveryAddress.DeliveryAddressRequestDto;
 import jihong99.shoppingmall.entity.DeliveryAddress;
 import jihong99.shoppingmall.entity.Users;
 import jihong99.shoppingmall.exception.NotFoundException;
-import jihong99.shoppingmall.mapper.DeliveryAddressMapper;
 import jihong99.shoppingmall.repository.DeliveryAddressRepository;
 import jihong99.shoppingmall.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Set;
 
@@ -23,51 +22,67 @@ public class DeliveryAddressServiceImpl implements IDeliveryAddressService {
     private final DeliveryAddressRepository deliveryAddressRepository;
     private final UserRepository userRepository;
 
-    /**
-     * Retrieves a set of delivery addresses for the specified user.
-     *
-     * @param findUser The user for whom the delivery addresses are to be retrieved.
-     * @return A set of delivery addresses associated with the user.
-     */
-    @Override
-    public Set<DeliveryAddress> getDeliveryAddresses(Users findUser) {
-        return deliveryAddressRepository.findByUsersId(findUser.getId());
-    }
 
     /**
-     * Adds a new delivery address for the specified user.
+     * Adds a new delivery address for a specific user.
      *
-     * @param requestDto The DTO object containing the delivery address details.
-     * @throws NotFoundException If the user with the specified ID is not found.
-     * @return Id of saved delivery address
+     * @param userId The ID of the user to whom the address will be added.
+     * @param requestDto The DTO containing the address details.
+     * @throws NotFoundException if the user is not found.
      */
     @Override
     @Transactional
-    public Long addDeliveryAddress(DeliveryAddressDto requestDto) {
-        Users findUser = userRepository.findById(requestDto.getUserId())
-                .orElseThrow(() -> new NotFoundException(MESSAGE_404_UserNotFound));
-        DeliveryAddressMapper deliveryAddressMapper = new DeliveryAddressMapper();
-        DeliveryAddress deliveryAddress = deliveryAddressMapper.mapToDeliveryAddress(findUser, requestDto);
-        return deliveryAddressRepository.save(deliveryAddress).getId();
-    }
-
-    /**
-     * Updates an existing delivery address with the specified details.
-     *
-     * @param addressId The ID of the delivery address to be updated.
-     * @param requestDto The DTO object containing the updated delivery address details.
-     * @throws NotFoundException If the delivery address with the specified ID is not found.
-     */
-    @Override
-    @Transactional
-    public void updateDeliveryAddress(Long addressId, DeliveryAddressDto requestDto) {
-        DeliveryAddress deliveryAddress = deliveryAddressRepository.findById(addressId)
-                .orElseThrow(() -> new NotFoundException(MESSAGE_404_DeliveryAddressNotFound));
-        applyAddressUpdates(requestDto, deliveryAddress);
+    public void addDeliveryAddress(Long userId, DeliveryAddressRequestDto requestDto) {
+        Users findUser = findUserOrThrow(userId);
+        DeliveryAddress deliveryAddress = getDeliveryAddress(requestDto, findUser);
         deliveryAddressRepository.save(deliveryAddress);
     }
 
-    private void applyAddressUpdates(DeliveryAddressDto requestDto, DeliveryAddress deliveryAddress) {
+    /**
+     * Updates an existing delivery address for a specific user.
+     *
+     * @param userId The ID of the user.
+     * @param addressId The ID of the address to update.
+     * @param requestDto The DTO containing the updated address details.
+     * @throws NotFoundException if the user or address is not found.
+     */
+    @Override
+    @Transactional
+    public void updateDeliveryAddress(Long userId, Long addressId, DeliveryAddressRequestDto requestDto) {
+        findUserOrThrow(userId);
+        DeliveryAddress deliveryAddress = findDeliveryAddressOrThrow(addressId);
+        updateDeliveryAddress(requestDto, deliveryAddress);
+        deliveryAddressRepository.save(deliveryAddress);
+    }
+
+    /**
+     * Deletes a delivery address for a specific user.
+     *
+     * @param userId The ID of the user.
+     * @param addressId The ID of the address to delete.
+     * @throws NotFoundException if the user or address is not found.
+     */
+    @Override
+    @Transactional
+    public void deleteDeliveryAddress(Long userId, Long addressId) {
+        findUserOrThrow(userId);
+        DeliveryAddress deliveryAddress = findDeliveryAddressOrThrow(addressId);
+        deliveryAddressRepository.delete(deliveryAddress);
+    }
+
+    /**
+     * Retrieves all delivery addresses associated with a specific user.
+     *
+     * @param findUser The user whose delivery addresses are being retrieved.
+     * @return A set of delivery addresses associated with the user.
+     */
+    @Override
+    public Set<DeliveryAddress> findDeliveryAddressesByUser(Users findUser) {
+        return deliveryAddressRepository.findAllByUsersId(findUser.getId());
+    }
+
+
+    private void updateDeliveryAddress(DeliveryAddressRequestDto requestDto, DeliveryAddress deliveryAddress) {
         deliveryAddress.updateAddress(requestDto.getAddress());
         deliveryAddress.updateAddressDetail(requestDto.getAddressDetail());
         deliveryAddress.updateZipCode(requestDto.getZipCode());
@@ -75,17 +90,25 @@ public class DeliveryAddressServiceImpl implements IDeliveryAddressService {
         deliveryAddress.updatePhoneNumber(requestDto.getPhoneNumber());
     }
 
-    /**
-     * Deletes the delivery address with the specified ID.
-     *
-     * @param addressId The ID of the delivery address to be deleted.
-     * @throws NotFoundException If the delivery address with the specified ID is not found.
-     */
-    @Override
-    @Transactional
-    public void deleteDeliveryAddress(Long addressId) {
-        DeliveryAddress deliveryAddress = deliveryAddressRepository.findById(addressId)
+
+    private DeliveryAddress findDeliveryAddressOrThrow(Long addressId) {
+        return deliveryAddressRepository.findById(addressId)
                 .orElseThrow(() -> new NotFoundException(MESSAGE_404_DeliveryAddressNotFound));
-        deliveryAddressRepository.delete(deliveryAddress);
+    }
+
+    private Users findUserOrThrow(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException(MESSAGE_404_UserNotFound));
+    }
+
+
+    private static DeliveryAddress getDeliveryAddress(DeliveryAddressRequestDto requestDto, Users findUser) {
+        return DeliveryAddress.createDeliveryAddress(
+                findUser,
+                requestDto.getName(),
+                requestDto.getPhoneNumber(),
+                requestDto.getZipCode(),
+                requestDto.getAddress(),
+                requestDto.getAddressDetail());
     }
 }
