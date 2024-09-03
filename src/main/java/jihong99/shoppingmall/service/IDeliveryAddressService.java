9070f562ad7113f579f9945259571cde1df93e98
jhmin99 <mjh012399@gmail.com>
2024-09-03 12:16:52 +0900

    refactor: Update IDeliveryAddressService to include user context in delivery address operations

--- src/main/java/jihong99/shoppingmall/service/IDeliveryAddressService.java
+++ src/main/java/jihong99/shoppingmall/service/IDeliveryAddressService.java
@@ -1,17 +1,19 @@
 package jihong99.shoppingmall.service;
 
-import jihong99.shoppingmall.dto.DeliveryAddressDto;
+
+import jihong99.shoppingmall.dto.request.deliveryAddress.DeliveryAddressRequestDto;
 import jihong99.shoppingmall.entity.DeliveryAddress;
 import jihong99.shoppingmall.entity.Users;
 
 import java.util.Set;
 
 public interface IDeliveryAddressService {
-    Set<DeliveryAddress> getDeliveryAddresses(Users findUser);
 
-    Long addDeliveryAddress(DeliveryAddressDto requestDto);
+    void addDeliveryAddress(Long userId, DeliveryAddressRequestDto deliveryAddressRequestDto);
 
-    void updateDeliveryAddress(Long addressId, DeliveryAddressDto requestDto);
+    void updateDeliveryAddress(Long userId, Long deliveryAddressId, DeliveryAddressRequestDto deliveryAddressRequestDto);
 
-    void deleteDeliveryAddress(Long addressId);
+    void deleteDeliveryAddress(Long userId, Long deliveryAddressId);
+
+    Set<DeliveryAddress> findDeliveryAddressesByUser(Users user);
 }
