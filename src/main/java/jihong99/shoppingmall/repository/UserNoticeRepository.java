9b0cfe8ce8364dbde341e1fc7d5cea7a3c0fbadf
jhmin99 <mjh012399@gmail.com>
2024-09-09 12:26:31 +0900

    feat: Add pagination and specific notice retrieval to UserNoticeRepository

--- src/main/java/jihong99/shoppingmall/repository/UserNoticeRepository.java
+++ src/main/java/jihong99/shoppingmall/repository/UserNoticeRepository.java
@@ -1,8 +1,15 @@
 package jihong99.shoppingmall.repository;
 
 import jihong99.shoppingmall.entity.UserNotice;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface UserNoticeRepository extends JpaRepository<UserNotice, Long> {
     void deleteAllByNoticeId(Long noticeId);
+    Page<UserNotice> findAllByUsersId(Long userId, Pageable pageable);
+
+    Optional<UserNotice> findByUsersIdAndNoticeId(Long userId, Long noticeId);
 }
