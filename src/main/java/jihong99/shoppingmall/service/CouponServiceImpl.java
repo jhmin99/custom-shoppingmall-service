93b062474ba220c4f0c9326354b820ee748dc20e
jhmin99 <mjh012399@gmail.com>
2024-09-30 20:24:03 +0900

    feat: Add methods for coupon summaries, details retrieval, and coupon application in ICouponService, CouponServiceImpl

--- src/main/java/jihong99/shoppingmall/service/CouponServiceImpl.java
+++ src/main/java/jihong99/shoppingmall/service/CouponServiceImpl.java
@@ -3,13 +3,19 @@ package jihong99.shoppingmall.service;
 import jihong99.shoppingmall.constants.Constants;
 import jihong99.shoppingmall.dto.request.coupon.CouponRequestDto;
 import jihong99.shoppingmall.dto.request.coupon.PatchCouponRequestDto;
+import jihong99.shoppingmall.dto.response.coupon.CouponDetailsResponseDto;
 import jihong99.shoppingmall.dto.response.coupon.CouponResponseDto;
+import jihong99.shoppingmall.dto.response.coupon.CouponSummaryResponseDto;
 import jihong99.shoppingmall.dto.response.coupon.UserCouponsResponseDto;
+import jihong99.shoppingmall.entity.Cart;
 import jihong99.shoppingmall.entity.Coupon;
 import jihong99.shoppingmall.entity.UserCoupon;
 import jihong99.shoppingmall.entity.Users;
+import jihong99.shoppingmall.entity.enums.DiscountType;
 import jihong99.shoppingmall.entity.enums.Roles;
+import jihong99.shoppingmall.exception.InvalidOperationException;
 import jihong99.shoppingmall.exception.NotFoundException;
+import jihong99.shoppingmall.repository.CartRepository;
 import jihong99.shoppingmall.repository.CouponRepository;
 import jihong99.shoppingmall.repository.UserCouponRepository;
 import jihong99.shoppingmall.repository.UserRepository;
@@ -19,6 +25,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,6 +36,7 @@ public class CouponServiceImpl implements ICouponService {
     private final CouponRepository couponRepository;
     private final UserRepository userRepository;
     private final UserCouponRepository userCouponRepository;
+    private final CartRepository cartRepository;
 
     /**
      * Retrieves the coupons of a specific user.
@@ -136,8 +144,84 @@ public class CouponServiceImpl implements ICouponService {
         }
         couponRepository.save(coupon);
     }
+    /**
+     * Retrieves all coupon summaries for a user and updates expired coupons to invalid.
+     *
+     * <p>This method checks each coupon's expiration date and updates its status to invalid if it has expired.</p>
+     *
+     * @param userId The ID of the user.
+     * @param status The status filter for coupons (e.g., 'valid', 'invalid').
+     * @param pageable Pagination information.
+     * @return A paginated list of coupon summaries.
+     */
+    @Transactional
+    @Override
+    public Page<CouponSummaryResponseDto> getAllCouponSummaries(Long userId, String status, Pageable pageable) {
+        Users user = findUserOrThrow(userId);
+        Page<UserCoupon> userCoupons = userCouponRepository.findAllByUsersIdAndStatus(user.getId(), status, pageable);
 
+        userCoupons.forEach(userCoupon -> {
+            if (userCoupon.getCoupon().getExpirationDate().isBefore(LocalDate.now())) {
+                userCoupon.updateToInvalid();
+                userCouponRepository.save(userCoupon);
+            }
+        });
 
+        return userCoupons.map(this::convertToCouponSummaryResponseDto);
+    }
+
+    /**
+     * Retrieves detailed information of a specific coupon for a user.
+     *
+     * @param userId The ID of the user.
+     * @param couponId The ID of the coupon.
+     * @return CouponDetailsResponseDto with coupon details.
+     */
+    @Transactional(readOnly = true)
+    @Override
+    public CouponDetailsResponseDto getCouponDetails(Long userId, Long couponId) {
+        UserCoupon userCoupon = findUserCouponOrThrow(userId, couponId);
+        return convertToCouponDetailsResponseDto(userCoupon);
+    }
+
+    /**
+     * Applies a coupon to a user's cart, adjusting the total price.
+     *
+     * @param userId The ID of the user.
+     * @param couponId The ID of the coupon to be applied.
+     */
+    @Override
+    @Transactional
+    public void applyCoupon(Long userId, Long couponId) {
+        UserCoupon userCoupon = findUserCouponOrThrow(userId, couponId);
+        Users user = findUserOrThrow(userId);
+        Cart cart = findCartOrThrow(user);
+        if (!userCoupon.getIsValid()) {
+            throw new InvalidOperationException(Constants.MESSAGE_400_InvalidCoupon);
+        }
+        if (userCoupon.getIsUsed()) {
+            throw new InvalidOperationException(Constants.MESSAGE_400_UsedCoupon);
+        }
+        if (cart.getAppliedCoupon() != null) {
+            cart.setEstimatedTotalPrice(cart.getOriginalTotalPrice());
+        }
+        cart.updateAppliedCoupon(userCoupon.getCoupon());
+        applyCouponDiscount(cart, userCoupon.getCoupon());
+
+        cartRepository.save(cart);
+    }
+    private Cart findCartOrThrow(Users user) {
+        return cartRepository.findById(user.getId()).orElseThrow(
+                () -> new NotFoundException(Constants.MESSAGE_404_CartNotFound)
+        );
+    }
+
+    private UserCoupon findUserCouponOrThrow(Long userId, Long couponId) {
+        return userCouponRepository.findByUsersIdAndCouponId(userId, couponId).orElseThrow(
+                () -> new NotFoundException(Constants.MESSAGE_404_UserCouponNotFound)
+        );
+    }
+    
     private Users findUserOrThrow(Long userId) {
         return userRepository.findById(userId).orElseThrow(
                 () -> new NotFoundException(Constants.MESSAGE_404_UserNotFound)
@@ -177,5 +261,41 @@ public class CouponServiceImpl implements ICouponService {
                 coupon.getExpirationDate(),
                 coupon.getCreationTime(),
                 coupon.getLastModifiedTime());
+    }
+
+    private CouponSummaryResponseDto convertToCouponSummaryResponseDto(UserCoupon userCoupon) {
+        return CouponSummaryResponseDto.of(
+                userCoupon.getId(),
+                userCoupon.getCoupon().getCode(),
+                userCoupon.getCoupon().getDiscountType(),
+                userCoupon.getCoupon().getDiscountValue(),
+                userCoupon.getCoupon().getExpirationDate()
+        );
+    }
+
+
+    private static CouponDetailsResponseDto convertToCouponDetailsResponseDto(UserCoupon userCoupon) {
+        return CouponDetailsResponseDto.of(
+                userCoupon.getId(),
+                userCoupon.getCoupon().getCode(),
+                userCoupon.getCoupon().getDiscountType(),
+                userCoupon.getCoupon().getDiscountValue(),
+                userCoupon.getIsValid(),
+                userCoupon.getIsUsed()
+        );
+    }
+
+
+
+    private void applyCouponDiscount(Cart cart, Coupon coupon) {
+        if (coupon.getDiscountType() == DiscountType.PERCENTAGE) {
+            cart.setEstimatedTotalPrice(cart.getOriginalTotalPrice() - calculatePercentageDiscount(cart, coupon));
+        } else if (coupon.getDiscountType() == DiscountType.FIXED) {
+            cart.setEstimatedTotalPrice(cart.getOriginalTotalPrice() - coupon.getDiscountValue());
+        }
+    }
+
+    private Long calculatePercentageDiscount(Cart cart, Coupon coupon) {
+        return cart.getOriginalTotalPrice() * coupon.getDiscountValue() / 100;
     }
 }
--- src/main/java/jihong99/shoppingmall/service/ICouponService.java
+++ src/main/java/jihong99/shoppingmall/service/ICouponService.java
@@ -3,7 +3,9 @@ package jihong99.shoppingmall.service;
 
 import jihong99.shoppingmall.dto.request.coupon.CouponRequestDto;
 import jihong99.shoppingmall.dto.request.coupon.PatchCouponRequestDto;
+import jihong99.shoppingmall.dto.response.coupon.CouponDetailsResponseDto;
 import jihong99.shoppingmall.dto.response.coupon.CouponResponseDto;
+import jihong99.shoppingmall.dto.response.coupon.CouponSummaryResponseDto;
 import jihong99.shoppingmall.dto.response.coupon.UserCouponsResponseDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -21,4 +23,9 @@ public interface ICouponService {
     void deleteCoupon(Long couponId);
     void patchCoupon(Long couponId, PatchCouponRequestDto patchCouponRequestDto);
 
+    Page<CouponSummaryResponseDto> getAllCouponSummaries(Long userId, String status, Pageable pageable);
+
+    CouponDetailsResponseDto getCouponDetails(Long userId, Long couponId);
+
+    void applyCoupon(Long userId, Long couponId);
 }
