136e1974f114e348b8e6b6ea7894291d056059dc
jhmin99 <mjh012399@gmail.com>
2024-09-30 20:24:49 +0900

    feat: Add methods to filter user coupons by status and retrieve coupon details in UserCouponRepository

--- src/main/java/jihong99/shoppingmall/repository/UserCouponRepository.java
+++ src/main/java/jihong99/shoppingmall/repository/UserCouponRepository.java
@@ -1,10 +1,25 @@
 package jihong99.shoppingmall.repository;
 
 import jihong99.shoppingmall.entity.UserCoupon;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 
-
+@Repository
 public interface UserCouponRepository extends JpaRepository<UserCoupon, Long> {
     void deleteAllByCouponId(Long couponId);
+
+    @Query("SELECT uc FROM UserCoupon uc WHERE uc.users.id = :userId " +
+            "AND ( " +
+            "(:status = 'available' AND uc.isValid = true AND uc.isUsed = false) OR " +
+            "(:status = 'used' AND uc.isUsed = true) OR " +
+            "(:status = 'invalid' AND uc.isValid = false) " +
+            ")")
+    Page<UserCoupon> findAllByUsersIdAndStatus(Long userId, String status, Pageable pageable);
+
+    Optional<UserCoupon> findByUsersIdAndCouponId(Long userId, Long couponId);
 }
