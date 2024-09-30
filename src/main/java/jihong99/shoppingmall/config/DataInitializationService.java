b5ddf31696262cf037b91d94e438b6c962b02899
jhmin99 <mjh012399@gmail.com>
2024-09-30 20:22:48 +0900

    refactor: Remove unused import statements from DataInitializationService

--- src/main/java/jihong99/shoppingmall/config/DataInitializationService.java
+++ src/main/java/jihong99/shoppingmall/config/DataInitializationService.java
@@ -1,18 +1,14 @@
 package jihong99.shoppingmall.config;
 
 import jakarta.transaction.Transactional;
-import jihong99.shoppingmall.entity.Coupon;
 import jihong99.shoppingmall.entity.Users;
-import jihong99.shoppingmall.repository.CouponRepository;
 import jihong99.shoppingmall.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 
-import static jihong99.shoppingmall.entity.enums.Roles.SUPER_ADMIN;
 
 @Service
 @RequiredArgsConstructor
