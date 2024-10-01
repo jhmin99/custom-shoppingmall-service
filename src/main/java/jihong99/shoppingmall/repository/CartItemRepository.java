e4b273e59b3e17c4b2d2a90c14d866df1ff498ef
jhmin99 <mjh012399@gmail.com>
2024-10-01 18:02:28 +0900

    feat: Add method to find CartItem by cartId and itemId

--- src/main/java/jihong99/shoppingmall/repository/CartItemRepository.java
+++ src/main/java/jihong99/shoppingmall/repository/CartItemRepository.java
@@ -5,7 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     List<CartItem> findByItemId(Long itemId);
+    Optional<CartItem> findByCartIdAndItemId(Long cartId, Long itemId);
 }
