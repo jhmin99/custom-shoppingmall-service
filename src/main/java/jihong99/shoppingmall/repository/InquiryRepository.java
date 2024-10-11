092b81d6cbb187b7aa0a95747f1467a756b5c60c
jhmin99 <mjh012399@gmail.com>
2024-10-11 21:46:32 +0900

    feat: Add user-specific inquiry retrieval to InquiryRepository

--- src/main/java/jihong99/shoppingmall/repository/InquiryRepository.java
+++ src/main/java/jihong99/shoppingmall/repository/InquiryRepository.java
@@ -8,7 +8,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
     Page<Inquiry> findAllByTypeAndStatus(InquiryType type, InquiryStatus status, Pageable pageable);
+
+    Page<Inquiry> findAllByUsersId(Long userId, Pageable pageable);
+
+    Optional<Inquiry> findByIdAndUsersId(Long inquiryId, Long userId);
 }
